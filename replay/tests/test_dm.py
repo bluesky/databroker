845a41ea8c7bc60d05fb485c0399e57cf3b07df9
Thomas A Caswell <tcaswell@bnl.gov>
2014-11-22 00:01:03 -0500

    TST : more test coverage for DataMuggler

--- replay/tests/test_dm.py
+++ replay/tests/test_dm.py
@@ -7,6 +7,8 @@ from replay.pipeline.pipeline import DataMuggler
 from datetime import datetime
 from nose.tools import assert_true, assert_equal
 from numpy.testing import assert_array_equal
+from nose.tools import assert_raises
+
 
 def test_maxframes():
     col_list = [('a', 'ffill', 0),
@@ -14,7 +16,7 @@ def test_maxframes():
                 ('c', 'ffill', 2)]
 
     dm = DataMuggler(col_list, max_frames=5)
-    print(dm.max_frames)
+
     for j in range(12):
         ts = datetime.now()
         data_dict = {'a': j, 'b': np.ones(2) * j,
@@ -77,3 +79,59 @@ def test_get_col():
     ts_lst, col_vals = dm.get_column('a')
     target_vals = [j * 2 + 1 for j in range(6)]
     assert_array_equal(col_vals, target_vals)
+
+    ts_lst, col_vals = dm.get_column('b')
+    for j, cv in zip(range(12), col_vals):
+        assert_array_equal(np.ones(2) * j, cv)
+
+    assert_raises(ValueError, dm.get_column, 'aardvark')
+
+
+def test_last_val():
+    col_list = [('a', 'ffill', 0),
+                ('b', 'ffill', 1),
+                ('c', 'ffill', 2)]
+
+    dm = DataMuggler(col_list)
+    ts_list = []
+    for j in range(12):
+        ts = datetime.now()
+        ts_list.append(ts)
+        data_dict = {'a': j, 'b': np.ones(2) * j,
+                     'c': np.ones((2, 2)) * j}
+
+        dm.append_data(ts, data_dict)
+
+        t, res = dm.get_last_value('a', ['b', 'c'])
+        assert_equal(ts, t)
+        for k in data_dict:
+            assert_array_equal(res[k], data_dict[k])
+
+
+def test_get_times():
+    col_list = [('a', 'ffill', 0),
+                ('b', 'ffill', 1),
+                ('c', 'ffill', 2)]
+
+    dm = DataMuggler(col_list)
+    ts_list = []
+    for j in range(12):
+        ts = datetime.now()
+        ts_list.append(ts)
+        data_dict = {'a': j, 'b': np.ones(2) * j,
+                     'c': np.ones((2, 2)) * j}
+
+        dm.append_data(ts, data_dict)
+    res_ts = dm.get_times('a')
+    assert_equal(ts_list, list(res_ts))
+
+
+def test_bad_col_append():
+    col_list = [('a', 'ffill', 0),
+                ('b', 'ffill', 1),
+                ('c', 'ffill', 2)]
+
+    dm = DataMuggler(col_list)
+
+    bad_dict = {'aardvark': 42}
+    assert_raises(ValueError, dm.append_data, datetime.now(), bad_dict)
