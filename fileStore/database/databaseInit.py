598fce28b77180e83c36e0d73e9111a156a366e5
arkilic <arkilic@bnl.gov>
2015-01-08 13:55:41 -0500

    ENH:More meaningful error returned if connection pool cannot be established

--- fileStore/database/databaseInit.py
+++ fileStore/database/databaseInit.py
@@ -2,7 +2,7 @@ __author__ = 'arkilic'
 
 from fileStore.conf import database, host, port
 from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
+from pymongo.errors import ConnectionFailure, PyMongoError
 
 #TODO: Add native python logger and pyOlog hook
 
@@ -10,6 +10,6 @@ try:
     conn = MongoClient(host=host, port=int(port))
     db = conn[database]
 except ConnectionFailure:
-    raise ConnectionFailure("Connection to Mongo server cannot be established. Make sure Mongo Daemon is running."
+    raise PyMongoError("Connection to Mongo server cannot be established. Make sure Mongo Daemon is running."
                             "Please check your host, port, and database configuration")
 
