bbb58a3be2f54412e1bfa49f2ea93b47048f88e2
Thomas A Caswell <tcaswell@bnl.gov>
2015-01-09 18:18:47 -0500

    PEP8 : white space changes
     - paramaterized error messages

--- fileStore/api/collection.py
+++ fileStore/api/collection.py
@@ -1,8 +1,16 @@
 __author__ = 'arkilic'
 
-from fileStore.database.databaseInit import db, conn
-from fileStore.database.mongoCollections import FileAttributes, FileBase, EventList
+from fileStore.database.databaseInit import db
+from fileStore.database.mongoCollections import (FileAttributes, FileBase,
+                                                 EventList)
 from pymongo.errors import OperationFailure, PyMongoError
+
+
+__all__ = ['create_file_link', 'create_file_attributes', 'create_event_list']
+
+
+_connection_err_msg = ("{} entry insert failde due to " +
+                       "database server connection")
 
 
 def create_file_link(file_id, spec, file_path, custom=None):
@@ -13,14 +21,17 @@ def create_file_link(file_id, spec, file_path, custom=None):
     :param custom:
     :return:
     """
-    #TODO: Validate file_id, file_path, and spec data types here
+    # TODO: Validate file_id, file_path, and spec data types here
     if custom is None:
         custom = dict()
-    f_base = FileBase(file_id=file_id, spec=spec, file_path=file_path, custom=custom)
+    f_base = FileBase(file_id=file_id, spec=spec,
+                      file_path=file_path, custom=custom)
     try:
-        flink_id = db['file_base'].insert(f_base.bsonify(), wtimeout=100, write_concern={'w': 1})
+        flink_id = db['file_base'].insert(f_base.bsonify(),
+                                          wtimeout=100, write_concern={'w': 1})
     except OperationFailure:
-        raise PyMongoError("File link entry insert failed due to database server connection")
+        raise PyMongoError(_connection_err_msg.format("File link"))
+
     return flink_id
 
 
@@ -34,18 +45,23 @@ def create_file_attributes(shape, dtype, **kwargs):
     """
     f_attr = FileAttributes(shape=shape, dtype=dtype, **kwargs)
     try:
-        f_attr_id = db['file_attributes'].insert(f_attr.bsonify(), wtimeout=100, write_concern={'w': 1})
+        f_attr_id = db['file_attributes'].insert(
+            f_attr.bsonify(), wtimeout=100, write_concern={'w': 1})
     except OperationFailure:
-        raise PyMongoError("File attributes entry insert failed due to database server connection")
+        raise PyMongoError(_connection_err_msg.format("File attributes"))
+
     return f_attr_id
+
 
 def create_event_list(event_id, file_id, event_list_custom=None):
     if event_list_custom is None:
         event_list_custom = dict()
-    event_list = EventList(event_id=event_id, file_id=file_id, event_list_custom=event_list_custom)
-    try:
-        event_list_id = db['event_list'].insert(event_list.bsonify(), wtimeout=100, write_concern={'w': 1})
-    except OperationFailure:
-        raise PyMongoError("File event list entry failed due to database server connection")
-    return event_list_id
 
+    event_list = EventList(event_id=event_id, file_id=file_id,
+                           event_list_custom=event_list_custom)
+    try:
+        event_list_id = db['event_list'].insert(
+            event_list.bsonify(), wtimeout=100, write_concern={'w': 1})
+    except OperationFailure:
+        raise PyMongoError(_connection_err_msg.format("File event list"))
+    return event_list_id
