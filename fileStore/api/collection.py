5879b9acf5c07432c84be126ffb791364210764a
Thomas A Caswell <tcaswell@bnl.gov>
2015-01-09 19:25:36 -0500

    MNT : major re-factor of database code
     - created an object to keep track of database/connection objects
     - moved useful functions to be methods of this object
     - create instance and bind it's methods as the functions exposed in
       'api.collection'

--- fileStore/api/collection.py
+++ fileStore/api/collection.py
@@ -1,67 +1,13 @@
 __author__ = 'arkilic'
 
-from fileStore.database.databaseInit import db
-from fileStore.database.mongoCollections import (FileAttributes, FileBase,
-                                                 EventList)
-from pymongo.errors import OperationFailure, PyMongoError
+from fileStore.database.databaseInit import FSDB
+from fileStore.conf import database, host, port
 
 
 __all__ = ['create_file_link', 'create_file_attributes', 'create_event_list']
 
+db_obj = FSDB(host, port, database)
 
-_connection_err_msg = ("{} entry insert failde due to " +
-                       "database server connection")
-
-
-def create_file_link(file_id, spec, file_path, custom=None):
-    """
-    :param file_id:
-    :param spec:
-    :param file_path:
-    :param custom:
-    :return:
-    """
-    # TODO: Validate file_id, file_path, and spec data types here
-    if custom is None:
-        custom = dict()
-    f_base = FileBase(file_id=file_id, spec=spec,
-                      file_path=file_path, custom=custom)
-    try:
-        flink_id = db['file_base'].insert(f_base.bsonify(),
-                                          wtimeout=100, write_concern={'w': 1})
-    except OperationFailure:
-        raise PyMongoError(_connection_err_msg.format("File link"))
-
-    return flink_id
-
-
-def create_file_attributes(shape, dtype, **kwargs):
-    """
-
-    :param shape:
-    :param dtype:
-    :param kwargs:
-    :return:
-    """
-    f_attr = FileAttributes(shape=shape, dtype=dtype, **kwargs)
-    try:
-        f_attr_id = db['file_attributes'].insert(
-            f_attr.bsonify(), wtimeout=100, write_concern={'w': 1})
-    except OperationFailure:
-        raise PyMongoError(_connection_err_msg.format("File attributes"))
-
-    return f_attr_id
-
-
-def create_event_list(event_id, file_id, event_list_custom=None):
-    if event_list_custom is None:
-        event_list_custom = dict()
-
-    event_list = EventList(event_id=event_id, file_id=file_id,
-                           event_list_custom=event_list_custom)
-    try:
-        event_list_id = db['event_list'].insert(
-            event_list.bsonify(), wtimeout=100, write_concern={'w': 1})
-    except OperationFailure:
-        raise PyMongoError(_connection_err_msg.format("File event list"))
-    return event_list_id
+create_event_list = db_obj.create_event_list
+create_file_attributes = db_obj.create_file_attributes
+create_file_link = db_obj.create_file_link
--- fileStore/database/databaseInit.py
+++ fileStore/database/databaseInit.py
@@ -1,15 +1,64 @@
 __author__ = 'arkilic'
 
-from fileStore.conf import database, host, port
+from .. import EID_KEY, FID_KEY
+
 from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure, PyMongoError
 
-#TODO: Add native python logger and pyOlog hook
+# TODO: Add native python logger and pyOlog hook
+from fileStore.database.mongoCollections import (FileAttributes, FileBase,
+                                                 EventList)
 
-try:
-    conn = MongoClient(host=host, port=int(port))
-    db = conn[database]
-except ConnectionFailure:
-    raise PyMongoError("Connection to Mongo server cannot be established. Make sure Mongo Daemon is running."
-                            "Please check your host, port, and database configuration")
 
+class FSDB(object):
+    def __init__(self, host, port, database):
+        self.conn = MongoClient(host=host, port=int(port))
+        self.db = self.conn[database]
+        self.db['event_list'].ensure_index(EID_KEY, unique=True)
+        self.db['file_base'].ensure_index(FID_KEY, unique=True)
+
+    _connection_err_msg = ("{} entry insert failde due to " +
+                           "database server connection")
+
+    def create_file_link(self, file_id, spec, file_path, custom=None):
+        """
+        :param file_id:
+        :param spec:
+        :param file_path:
+        :param custom:
+        :return:
+        """
+        # TODO: Validate file_id, file_path, and spec data types here
+        if custom is None:
+            custom = dict()
+        f_base = FileBase(file_id=file_id, spec=spec,
+                          file_path=file_path, custom=custom)
+        flink_id = self.db['file_base'].insert(f_base.bsonify(),
+                                                   wtimeout=100,
+                                                   write_concern={'w': 1})
+
+        return flink_id
+
+    def create_file_attributes(self, shape, dtype, **kwargs):
+        """
+
+        :param shape:
+        :param dtype:
+        :param kwargs:
+        :return:
+        """
+        f_attr = FileAttributes(shape=shape, dtype=dtype, **kwargs)
+        f_attr_id = self.db['file_attributes'].insert(
+            f_attr.bsonify(), wtimeout=100, write_concern={'w': 1})
+
+        return f_attr_id
+
+    def create_event_list(self, event_id, file_id, event_list_custom=None):
+        if event_list_custom is None:
+            event_list_custom = dict()
+
+        event_list = EventList(event_id=event_id, file_id=file_id,
+                               event_list_custom=event_list_custom)
+        event_list_id = self.db['event_list'].insert(
+                event_list.bsonify(), wtimeout=100, write_concern={'w': 1})
+
+        return event_list_id
