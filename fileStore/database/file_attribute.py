273dafcac6449e7b2096c75a054b7eb2ac1ee6d8
Arman Arkilic <arkilic@bnl.gov>
2015-01-14 10:08:17 -0500

    MNT: Added FileBase class for document template into file_base collection

--- fileStore/database/file_attribute.py
+++ fileStore/database/file_attribute.py
@@ -1,1 +1,15 @@
 __author__ = 'arkilic'
+
+from mongoengine import Document, FloatField, DateTimeField, StringField, DictField, ReferenceField
+from fileStore.database.file_base import FileBase
+
+
+class FileAttributes(Document):
+    """
+
+    Parameters
+    ----------
+
+    """
+    
+
--- fileStore/database/file_base.py
+++ fileStore/database/file_base.py
@@ -1,1 +1,26 @@
 __author__ = 'arkilic'
+
+
+from mongoengine import Document, StringField, DictField
+
+
+class FileBase(Document):
+    """
+
+    Parameters
+    ---------
+
+    spec: str
+    File spec used to primarily parse the contents into analysis environment
+
+    file_path: str
+    Url to the physical location of the file
+
+    custom: dict
+    custom name/value container in case additional info save is required
+
+    """
+
+    spec = StringField(max_lenght=10, required=True, unique=False)
+    file_path = StringField(max_length=100, required=True, unique=False)
+    custom = DictField(required=False)
