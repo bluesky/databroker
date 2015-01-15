d375224ec289a35fdb63f5e01ac966299aee965d
Arman Arkilic <arkilic@bnl.gov>
2015-01-15 09:37:29 -0500

    WIP: Added save routines to commands. Some minor modifications to classes and a few to-do items

--- fileStore/database/file_event_link.py
+++ fileStore/database/file_event_link.py
@@ -6,6 +6,19 @@ from fileStore.database import file_base
 
 
 class FileEventLink(Document):
-    file_id = ReferenceField(file_base.FileBase, reverse_delete_rule=DENY, required=True)
+    """
+    Parameters
+    ----------
+
+    file_id:
+
+    event_id:
+
+    link_parameters:
+
+    """
+    file_base = ReferenceField(file_base.FileBase, reverse_delete_rule=DENY, required=True)
     event_id = StringField(required=True)
     link_parameters = DictField(required=False)
+#TODO: add indexing
+#TODO: add documentation
