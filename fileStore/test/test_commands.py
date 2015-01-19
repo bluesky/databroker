9cba3c70f22f4f9c74a6eb5ad12c2b3fb89cd696
Thomas A Caswell <tcaswell@bnl.gov>
2015-01-18 22:41:04 -0500

    TST : replace smoke test with round-trip test

--- fileStore/test/test_commands.py
+++ fileStore/test/test_commands.py
@@ -1,3 +1,8 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import six
+import numpy as np
 import uuid
 import mongoengine
 import mongoengine.connection
@@ -5,21 +10,31 @@ from nose.tools import make_decorator
 from mongoengine.context_managers import switch_db
 
 import fileStore.commands as fc
+import fileStore.retrieve as fsr
 from fileStore.database.file_base import FileBase
 from fileStore.database.file_event_link import FileEventLink
+from numpy.testing import assert_array_equal
+
+from .t_utils import SynHandlerMod, SynHandlerEcho
 
 db_name = str(uuid.uuid4())
-# need to make 'default' connection to point to no-where, just to be safe
-conn = mongoengine.connect(str(uuid.uuid4()))
+dummy_db_name = str(uuid.uuid4())
 
 
 def setup():
+    # need to make 'default' connection to point to no-where, just to be safe
+    mongoengine.connect(dummy_db_name)
+    # connect to the db we are actually going to use
     mongoengine.connect(db_name, alias='test_db')
+    # register the dummy handler to use
+    fsr.register_handler('syn-mod', SynHandlerMod)
 
 
 def teardown():
     conn = mongoengine.connection.get_connection('test_db')
     conn.drop_database(db_name)
+    conn.drop_database(dummy_db_name)
+    del fsr._h_registry['syn-mod']
 
 
 def context_decorator(func):
@@ -31,12 +46,23 @@ def context_decorator(func):
     return make_decorator(func)(inner)
 
 
-@context_decorator
-def test_smoke():
-    for j in range(10):
+def _insert_syn_data(f_type, shape, count):
+    fb = fc.save_file_base(f_type, '',
+                           {'shape': shape})
+    ret = []
+    for k in range(count):
+        r_id = str(uuid.uuid4())
+        fc.save_file_event_link(fb, r_id, {'n': k + 1})
+        ret.append(r_id)
+    return ret
 
-        fb = fc.save_file_base('syn-mod', '',
-                               {'shape': (50, 70)})
-        for k in range(10):
-            fc.save_file_event_link(fb,
-                                    '{}_{}'.format(j, k), {'n': k})
+
+@context_decorator
+def test_round_trip():
+    shape = (25, 32)
+    mod_ids = _insert_syn_data('syn-mod', shape, 10)
+
+    for j, r_id in enumerate(mod_ids):
+        data = fc.retrieve_data(r_id)
+        known_data = np.mod(np.arange(np.prod(shape)), j + 1).reshape(shape)
+        assert_array_equal(data, known_data)
