36b0551bdfef252ede07cef0aaaf32caf054f6a7
danielballan <daniel.b.allan@gmail.com>
2015-01-20 12:28:39 -0500

    WIP: Flesh out find2.

--- databroker/sources/dummy_sources/_metadataStore/api/analysis.py
+++ databroker/sources/dummy_sources/_metadataStore/api/analysis.py
@@ -1,2 +1,10 @@
-def find2():
-    pass
+headers = []
+beamline_configs = []
+event_descriptors = []
+events = []
+
+
+def find2(header_id=None, scan_id=None, owner=None, start_time=None,
+          beamline_id=None, end_time=None):
+    return {'headers': headers, 'beamline_configs': beamline_configs,
+            'event_descriptors': event_descriptors, 'events': events}
