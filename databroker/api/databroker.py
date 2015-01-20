083c7ed0f3deb9922e83fe3d26899c25d0467dc6
danielballan <daniel.b.allan@gmail.com>
2015-01-20 12:41:46 -0500

    WIP: Actually get data.

--- databroker/api/databroker.py
+++ databroker/api/databroker.py
@@ -1,8 +1,10 @@
+from __future__ import print_function
 import six
+from ..api.data import Data
 from .. import validations
 from .. import sources
-
-find2 = sources.metadataStore.api.analysis.find2
+# Note: Invoke contents of sources at the func/method level so that it
+# respects runtime switching between real and dummy sources.
 
 
 class DataBroker(object):
@@ -22,4 +24,7 @@ class DataBroker(object):
 
     def search(self, start_time, end_time):
         "Get events from the MDS between these two times."
+        find2 = sources.metadataStore.api.analysis.find2
         metadata = find2(start_time, end_time, beamline_id=self.beamline_id)
+        print(metadata)
+        return Data()
