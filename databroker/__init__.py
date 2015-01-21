c41786bae5640511591c919ed26d789caf9d68fb
danielballan <daniel.b.allan@gmail.com>
2015-01-21 16:13:11 -0500

    MNT: Remove default handler.

--- databroker/__init__.py
+++ databroker/__init__.py
@@ -3,12 +3,4 @@ import logging
 from .sources import *
 
 
-# Attach a default handler, printing INFO-level logs to stdout.
 logger = logging.getLogger(__name__)
-FORMAT = "%(name)s.%(funcName)s:  %(message)s"
-formatter = logging.Formatter(FORMAT)
-default_handler = logging.StreamHandler(sys.stdout)
-default_handler.setLevel(logging.INFO)
-default_handler.setFormatter(formatter)
-logger.addHandler(default_handler)
-logger.setLevel(logging.INFO)
