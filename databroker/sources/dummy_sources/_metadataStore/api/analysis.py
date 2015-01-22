d57bf8b0995496d8cabc81410154ab64a0673e01
danielballan <daniel.b.allan@gmail.com>
2015-01-22 13:52:28 -0500

    Update dummy MDS to use find and return a list of events.

--- databroker/sources/dummy_sources/_metadataStore/api/analysis.py
+++ databroker/sources/dummy_sources/_metadataStore/api/analysis.py
@@ -1,10 +1,22 @@
-headers = []
-beamline_configs = []
-event_descriptors = []
-events = []
+from datetime import datetime as dt
 
 
-def find2(header_id=None, scan_id=None, owner=None, start_time=None,
-          beamline_id=None, end_time=None):
-    return {'headers': headers, 'beamline_configs': beamline_configs,
-            'event_descriptors': event_descriptors, 'events': events}
+class DummyEventDescriptor(object):
+    def __init__(self):
+        self.keys = {'temp': {'source': 'PV:blah'},
+                     'picture': {'source': 'CCD:blah',
+                                 'external': 'FILESTORE!!!!'}}
+
+
+class DummyEvent(object):
+    def __init__(self):
+        self.ev_desc = DummyEventDescriptor()
+        self.data = {'temp': {'value': 273, 'timestamp': None},
+                     'picture': {'value': 'np.ones((10, 10))',
+                                 'timestamp': None}}
+        self.time = dt(2014, 01, 01, 1, 2, 3)
+
+
+def find(header_id=None, scan_id=None, owner=None, start_time=None,
+         beamline_id=None, end_time=None):
+    return 3 * [DummyEvent()]
