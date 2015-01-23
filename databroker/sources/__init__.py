5106e9680d3938b63177d9facc9b5d9b2432e83b
Thomas A Caswell <tcaswell@gmail.com>
2015-01-22 19:16:55 -0500

    MNT : make mds.api.collection gets imported

--- databroker/sources/__init__.py
+++ databroker/sources/__init__.py
@@ -5,7 +5,8 @@ import logging
 
 logger = logging.getLogger(__name__)
 source_names = ['channelarchiver', 'metadataStore', 'metadataStore.api',
-                'metadataStore.api.analysis', 'fileStore', 'fileStore.commands']
+                'metadataStore.api.analysis', 'metadataStore.api.collection',
+                'fileStore', 'fileStore.commands']
 __all__ = ['switch'] + source_names
 
 
