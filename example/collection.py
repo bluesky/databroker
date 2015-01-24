744635af431f4299ec241acd163be4fdfba57df4
arkilic <arkilic@bnl.gov>
2015-01-24 12:30:39 -0500

    WIP: Added event creation to example/collection.py

--- example/collection.py
+++ example/collection.py
@@ -19,3 +19,5 @@ ed = save_event_descriptor(event_type_id=1, data_keys=['arm.an', 'arkilic'], des
 h = save_header(unique_id=str(id_generator(5)), scan_id=3,  create_time=time.time(), beamline_config=b,
                 event_descriptor=ed,
                 custom={'data':123})
+
+save_event(header=h, event_descriptor=ed, seq_no=1, timestamp=time.time(), data={'arm.an': 1, 'arkilic': 5})
