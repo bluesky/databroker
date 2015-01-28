2321990d1280af53c96e026981c0ce565f329473
Thomas A Caswell <tcaswell@gmail.com>
2015-01-28 17:43:09 -0500

    ENH : add 'save_ndarray' to analysis api
     - also register the npyreader as part of the import process of
       fs.api

--- fileStore/api/__init__.py
+++ fileStore/api/__init__.py
@@ -1,1 +1,12 @@
 __author__ = 'arkilic'
+
+# import to do registering so things 'just work'
+from .. import retrieve as fsr
+from ..file_readers import NpyHandler
+
+# register npy handler
+fsr.register_handler('npy', NpyHandler)
+
+# clean up to not pollute namespace
+del fsr
+del NpyHandler
