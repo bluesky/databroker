a80f41d9441f6bc4ea446c4a45e103215da98081
danielballan <daniel.b.allan@gmail.com>
2015-01-28 08:22:29 -0500

    ENH: Expand broker interface and MDS.

--- databroker/broker/simple_broker.py
+++ databroker/broker/simple_broker.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import six  # noqa
-from collections import defaultdict
+from collections import defaultdict, Iterable
 from .. import sources
 # Note: Invoke contents of sources at the func/method level so that it
 # respects runtime switching between real and dummy sources.
@@ -12,17 +12,47 @@ from .. import sources
 POPULAR_CHANNELS = ['SR11BCM01:LIFETIME_MONITOR', 'SR11BCM01:CURRENT_MONITOR']
 
 
-def search(beamline_id, start_time, end_time, ca_host, channels=None):
+def get_events_by_run(runs, ca_host, channels=None):
     """
-    Get data from all events from a given beamline between two times.
+    Get Events from given run(s).
 
     Parameters
     ----------
-    beamline_id : string
-        e.g., 'srx'
-    start_time : string or datetime object
+    args : one or a list of BeginRun or EndRun objects
+    ca_host : URL string
+        the URL of your archiver's ArchiveDataServer.cgi. For example,
+        'http://cr01arc01/cgi-bin/ArchiveDataServer.cgi'
+    channels : list, optional
+        All queries will return applicable data from the N most popular
+        channels. If data from additional channels is needed, their full
+        identifiers (not human-readable names) must be given here as a list
+        of strings.
+
+    Returns
+    -------
+    data : list of Event objects
+    """
+    find_event = sources.metadataStore.api.analysis.find_event
+
+    # Normalize input: runs is a list of BeginRun and/or EndRun objects.
+    if not isinstance(runs, Iterable):
+        runs = [runs]
+
+    events = [find_event(run) for run in runs]
+    events = [e for run in events for e in run]  # flattened
+    [_fill_event(event) for event in events]
+    return events
+
+
+def get_events_in_time_range(start_time, end_time, ca_host, channels=None):
+    """
+    Get Events that occurred between two times.
+
+    Parameters
+    ----------
+    start_time : string or datetime object, optional
         e.g., datetime.datetime(2015, 1, 1) or '2015-01-01' (ISO format)
-    end_time : string or datetime object
+    end_time : string or datetime object, optional
         e.g., datetime.datetime(2015, 1, 1) or '2015-01-01' (ISO format)
     ca_host : URL string
         the URL of your archiver's ArchiveDataServer.cgi. For example,
@@ -35,24 +65,31 @@ def search(beamline_id, start_time, end_time, ca_host, channels=None):
 
     Returns
     -------
-    data : list
-        See example below illustrating the format of the returned dataset.
-
-    Example
-    -------
-    >>> search('srx', '2015-01-01', '2015-01-02')
-    [(<unix epoch time>, {'chan1': <value>, 'chan2': <value>},
-     (<unix epoch time>, {'temp': <value>)}
-
-    That is, it results a list of tuples, where each tuple contains a time and
-    a dictionary of name/value pairs. Every value is guaranteed to be either a
-    scalar Python primitive (int, float, string) or a numpy ndarray.
+    data : list of Event objects
     """
-    # Get data and populate external references from the File Store.
-    find = sources.metadataStore.api.analysis.find
-    events = find(start_time=start_time, end_time=end_time,
-                  beamline_id=beamline_id)
-    data = [_parse_event(event) for event in events]
+    raise NotImplementedError()
+
+
+def explore(**kwargs):
+    """
+    For now, pass through to metadataStore.api.analysis.find_header
+
+    Parameters
+    ----------
+    **kwargs
+
+    Returns
+    -------
+    data : list
+        Header objects
+    """
+    find_header = sources.metadataStore.api.analysis.find_header
+    return find_header(**kwargs)
+
+
+def _assmeble_output(events):
+    "Used by get_event_* functions"
+    data = [_scrape_event(event) for event in events]
 
     # Get data from commonly-used Channel Archiver channels plus any
     # specified in the call.
@@ -71,6 +108,10 @@ def search(beamline_id, start_time, end_time, ca_host, channels=None):
 
 
 def _inspect_descriptor(descriptor):
+    """
+    Return a dict with the data keys mapped to boolean answering whether
+    data is external.
+    """
     # TODO memoize to cache these results
     data_keys = descriptor.keys
     is_external = defaultdict(lambda: False)
@@ -80,7 +121,20 @@ def _inspect_descriptor(descriptor):
     return is_external
 
 
-def _parse_event(event):
+def _fill_event(event):
+    """
+    Populate events with externally stored data.
+    """
+    retrieve_data = sources.fileStore.commands.retrieve_data
+    is_external = _inspect_descriptor(event.ev_desc)
+    for data_key, data_dict in event.data.items():
+        value, _ = data_dict['value'], data_dict['timestamp']
+        if is_external[data_key]:
+            # Retrieve a numpy array from filestore
+            event.data[data_key] = retrieve_data(value)
+
+
+def _scrap_event(event):
     """
     Return event.time, data where data is a dict of field names and values.
 
