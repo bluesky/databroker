77e7f0da62e4aa6d19444fd84390b02c31c4cdfd
arkilic <arkilic@bnl.gov>
2015-01-29 20:56:37 -0500

    ENH: New commands added to analysis api namespace

--- metadataStore/api/analysis.py
+++ metadataStore/api/analysis.py
@@ -2,5 +2,5 @@ __author__ = 'arkilic'
 
 
 from ..commands import find, find_event, find_event_descriptor, find_beamline_config, find_begin_run, find_last, \
-    find_event_given_descriptor
+    find_event_given_descriptor, fetch_events
 
