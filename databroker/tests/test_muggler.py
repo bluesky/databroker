2a57a4bff6bade8d273aadb83e2d986aee6701ed
danielballan <daniel.b.allan@gmail.com>
2015-01-30 21:58:50 -0500

    Run muggler tests on events from example.

--- databroker/tests/test_muggler.py
+++ databroker/tests/test_muggler.py
@@ -4,62 +4,24 @@ import numpy as np
 from .. import sources
 from ..muggler.data import DataMuggler
 from ..sources import switch
+from ..examples.seeding_data import temperature_ramp
 
 
 class TestMuggler(unittest.TestCase):
 
     def setUp(self):
-        switch(metadatastore=True)
-        insert_begin_run = sources.metadataStore.api.collection.insert_begin_run
-        insert_event_descriptor = \
-            sources.metadataStore.api.collection.insert_event_descriptor
-        insert_event = sources.metadataStore.api.collection.insert_event
-
-        data_keys = {'linear_motor': {'source': 'PV1'},
-                     'scalar_detector': {'source': 'PV2'},
-                     'Tsam': {'source': 'PV3'}}
-
-        bre = insert_begin_run(time.time(), 'csx')
-
-        ev_desc = insert_event_descriptor(time.time(), data_keys=data_keys,
-                                          begin_run_event=bre)
-
-        func = np.cos
-        num = 100
-        start = 0
-        stop = 10
-        sleep_time = .05
-
-        events = []
-        for idx, i in enumerate(np.linspace(start, stop, num)):
-            data = {'linear_motor': i,
-                    'Tsam': i + 5,
-                    'scalar_detector': func(i)}
-            event = insert_event(ev_desc, time.time() + i, data, i)
-            events.append(event)
-        time.sleep(sleep_time)
-
-        self.events = events
-        self.ev_desc = ev_desc
-        self.bre = bre
-        self.data_keys = data_keys
+        self.mixed_events = temperature_ramp.run()
 
     def test_empty_muggler(self):
         DataMuggler()
 
     def test_from_events(self):
-        DataMuggler.from_events(self.events)
+        DataMuggler.from_events(self.mixed_events)
 
     def test_dataframe(self):
-        dm = DataMuggler.from_events(self.events)
+        dm = DataMuggler.from_events(self.mixed_events)
         dm._dataframe
 
     def test_binning(self):
-        dm = DataMuggler.from_events(self.events)
+        dm = DataMuggler.from_events(self.mixed_events)
         dm.bin_by_edges([(1, 2), (3, 4)])  # TODO more relavant bins
-
-    def test_attributes(self):
-        dm = DataMuggler.from_events(self.events)
-        sources = dm.sources
-        expected_sources = {k: v['source'] for k, v in self.data_keys.items()}
-        self.assertEqual(sources, expected_sources)
