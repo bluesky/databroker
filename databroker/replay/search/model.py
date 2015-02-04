215f6fc221eb0018190f7debdbfca743bba4a56c
Eric Dill <edill@bnl.gov>
2015-02-04 17:10:31 -0500

    ENH: Presentation of Run Summary and Events

--- databroker/replay/search/model.py
+++ databroker/replay/search/model.py
@@ -1,8 +1,11 @@
 """Module that defines the Atom Models that back the Search Views"""
 
-from atom.api import Atom, Typed, List, Range, Dict, observe
+import six
+from collections import OrderedDict
+from atom.api import Atom, Typed, List, Range, Dict, observe, Str
 from databroker.api import BrokerStruct
 from databroker.broker import simple_broker
+from pprint import pprint
 
 class LastModel(Atom):
     """Class that defines the model for the 'get last N datasets view'
@@ -17,17 +20,40 @@ class LastModel(Atom):
     begin_run_events = List()
     selected = Typed(BrokerStruct)
     selected_as_dict = Dict()
+    selected_keys = Dict()
+    __begin_run_events_as_dict = Dict()
+    __begin_run_events_keys = Dict()
 
     @observe('selected')
     def selected_changed(self, changed):
-        self.selected_as_dict = {'id': 'None'}
-        try:
-            self.selected_as_dict = vars(self.selected)
-        except TypeError:
-            # do nothing, because selected_as_dict was already set to an
-            # empty dictionary at the top of this function
-            pass
+        # set the summary dictionary
+        self.selected_as_dict = {}
+        self.selected_as_dict = self.__begin_run_events_as_dict[self.selected]
+        # set the keys dictionary
+        self.selected_keys = {}
+        self.selected_keys = self.__begin_run_events_keys[self.selected]
 
     @observe('num_to_retrieve')
     def num_changed(self, changed):
         self.begin_run_events = simple_broker.get_last_headers(self.num_to_retrieve)
+        begin_run_events_as_dict = {}
+        begin_run_events_keys = {}
+        for bre in self.begin_run_events:
+            bre_vars = vars(bre)
+            event_descriptors = bre_vars.pop('event_descriptors', [])
+            sample = bre_vars.pop('sample', {})
+            beamline_config = bre_vars.pop('beamline_config', {})
+            dct = bre_vars
+            begin_run_events_as_dict[bre] = dct
+            data_keys = {}
+            for evd in event_descriptors:
+                dk = evd.data_keys
+                for k, v in six.iteritems(dk):
+                    while k in dct:
+                        k += '_1'
+                    data_keys[k] = v
+            begin_run_events_keys[bre] = data_keys
+
+        self.__begin_run_events_as_dict = begin_run_events_as_dict
+        self.__begin_run_events_keys = begin_run_events_keys
+
