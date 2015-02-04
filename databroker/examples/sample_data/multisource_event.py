56e7f1db498331dc0a126d3080bc3dd884af8657
danielballan <daniel.b.allan@gmail.com>
2015-02-04 16:21:13 -0500

    TST: Add example and test with an event containing multiple data sources.

---
+++ databroker/examples/sample_data/multisource_event.py
@@ -0,0 +1,51 @@
+from __future__ import division
+from metadataStore.api.collection import (insert_event,
+                                          insert_event_descriptor)
+from ...broker.struct import BrokerStruct
+import numpy as np
+from . import common
+
+# "Magic numbers" for this simulation
+start, stop, step, points_per_step = 0, 3, 1, 7
+deadband_size = 0.9
+num_exposures = 23
+
+@common.example
+def run(begin_run=None):
+    # Make the data
+    ramp = common.stepped_ramp(start, stop, step, points_per_step)
+    deadbanded_ramp = common.apply_deadband(ramp, deadband_size)
+    rs = np.random.RandomState(5)
+    point_det_data = rs.randn(num_exposures)
+
+    # Create Event Descriptors
+    data_keys1 = {'point_det': {'source': 'PV:ES:PointDet'}}
+    data_keys2 = {'Tsam': {'source': 'PV:ES:Tsam'},
+                  'Troom': {'source': 'PV:ES:Troom'}}
+    ev_desc1 = insert_event_descriptor(begin_run_event=begin_run,
+                                       data_keys=data_keys1, time=0.)
+    ev_desc2 = insert_event_descriptor(begin_run_event=begin_run,
+                                       data_keys=data_keys2, time=0.)
+
+    # Create Events.
+    events = []
+
+    # Point Detector Events
+    for i in range(num_exposures):
+        time = float(i + 0.01 * rs.randn())
+        data = {'point_det': {'value': point_det_data[i], 'timestamp': time}}
+        event = insert_event(event_descriptor=ev_desc1, seq_no=i, time=time,
+                             data=data)
+        events.append(event)
+
+    # Temperature Events
+    for i, (time, temp) in enumerate(zip(*deadbanded_ramp)):
+        time = float(time)
+        data = {'Tsam': {'value': temp, 'timestamp': time},
+                'Troom': {'value': temp + 10, 'timestamp': time}}
+        event = insert_event(event_descriptor=ev_desc2, time=time,
+                             data=data, seq_no=i)
+        events.append(event)
+
+    events = [BrokerStruct(event) for event in events]
+    return events
--- databroker/tests/test_muggler.py
+++ databroker/tests/test_muggler.py
@@ -6,14 +6,13 @@ import numpy as np
 from .. import sources
 from ..muggler.data import DataMuggler, BinningError
 from ..sources import switch
-from ..examples.sample_data import temperature_ramp
+from ..examples.sample_data import temperature_ramp, multisource_event
 
 
 class TestMuggler(unittest.TestCase):
 
     def setUp(self):
         self.mixed_scalars = temperature_ramp.run()
-        self.dm = DataMuggler.from_events(self.mixed_scalars)
 
     def test_empty_muggler(self):
         DataMuggler()
@@ -25,40 +24,57 @@ class TestMuggler(unittest.TestCase):
         dm.Tsam
         dm['Tsam']
 
-    def test_bin_on(self):
-        sparse_col_name = 'Tsam'
-        dense_col_name = 'point_det'
 
-        # ALIGNING A DENSE COLUMN TO A SPARSE COLUMN
+class CommonBinningTests(object):
+
+    def test_bin_on_sparse(self):
+        "Align a dense column to a sparse column."
 
         # If downsampling is necessary but there is no rule, fail.
-        bad_binning = lambda: self.dm.bin_on(sparse_col_name)
+        bad_binning = lambda: self.dm.bin_on(self.sparse)
         self.assertRaises(BinningError, bad_binning)
 
         # With downsampling, the result should have as many entires as the
         # data source being "binned on" (aligned to).
-        actual_len = len(self.dm.bin_on(sparse_col_name,
-                                        agg={dense_col_name: np.mean}))
-        expected_len = len(self.dm[sparse_col_name])
+        actual_len = len(self.dm.bin_on(self.sparse,
+                                        agg={self.dense: np.mean}))
+        expected_len = len(self.dm[self.sparse])
         self.assertEqual(actual_len, expected_len)
 
-        # ALIGNING A SPARSE COLUMNS TO A DENSE COLUMN
+    def test_bin_on_dense(self):
+        "Align a sparse column to a dense column"
 
         # With or without upsampling, the result should have the sample
         # length as the dense column.
-        expected_len = len(self.dm[dense_col_name])
-        result1 = self.dm.bin_on(dense_col_name)
+        expected_len = len(self.dm[self.dense])
+        result1 = self.dm.bin_on(self.dense)
         actual_len1 = len(result1)
         self.assertEqual(actual_len1, expected_len)
-        result2 = self.dm.bin_on(dense_col_name,
-                                 interpolation={sparse_col_name: 'linear'})
+        result2 = self.dm.bin_on(self.dense,
+                                 interpolation={self.sparse: 'linear'})
         actual_len2 = len(result2)
         self.assertEqual(actual_len2, expected_len)
 
         # If there is an interpolation rule, there should be no missing values
         # except (perhaps) at the edges outside the domain of the sparse col.
-        first = self.dm[sparse_col_name].first_valid_index()
-        last = self.dm[sparse_col_name].last_valid_index()
-        expected_len = 2 + len(self.dm[dense_col_name].loc[first:last])
-        self.assertLess(result1[sparse_col_name].count(), expected_len)
-        self.assertEqual(result2[sparse_col_name].count(), expected_len)
+        first = self.dm[self.sparse].first_valid_index()
+        last = self.dm[self.sparse].last_valid_index()
+        expected_len = 2 + len(self.dm[self.dense].loc[first:last])
+        self.assertLess(result1[self.sparse].count(), expected_len)
+        self.assertEqual(result2[self.sparse].count(), expected_len)
+
+
+class TestBinningTwoScalarEvents(CommonBinningTests, unittest.TestCase):
+
+    def setUp(self):
+        self.dm = DataMuggler.from_events(temperature_ramp.run())
+        self.sparse = 'Tsam'
+        self.dense = 'point_det'
+
+
+class TestBinningMultiSourceEvents(CommonBinningTests, unittest.TestCase):
+
+    def setUp(self):
+        self.dm = DataMuggler.from_events(multisource_event.run())
+        self.sparse = 'Troom'
+        self.dense = 'point_det'
