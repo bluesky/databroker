b960657028e2cb4593a7e137ee96d72b7ab0fa03
Thomas A Caswell <tcaswell@bnl.gov>
2015-02-11 11:58:42 -0500

    PEP8 : white space changes

--- fileStore/commands.py
+++ fileStore/commands.py
@@ -40,9 +40,10 @@ def save_file_base(spec, file_path, custom=None, collection_version=0):
         custom name/value container in case additional info save is required
 
     """
-    connect(db=database, host=host, port=port)
 
-    file_base_object = FileBase(spec=spec, file_path=file_path, custom=custom, collection_version=collection_version)
+    file_base_object = FileBase(spec=spec, file_path=file_path,
+                                custom=custom,
+                                collection_version=collection_version)
 
     file_base_object.save(validate=True, write_concern={"w": 1})
 
@@ -63,21 +64,23 @@ def save_file_attributes(file_base, shape, dtype,
 
     """
 
-    connect(db=database, host=host, port=port)
-
-    file_attributes = FileAttributes(file_base=file_base.id, shape=shape, dtype=dtype,
+    file_attributes = FileAttributes(file_base=file_base.id, shape=shape,
+                                     dtype=dtype,
                                      collection_version=collection_version)
 
     file_attributes.total_bytes = kwargs.pop('total_bytes', None)
     file_attributes.hashed_data = kwargs.pop('hashed_data', None)
     file_attributes.last_access = kwargs.pop('last_access', None)
-    file_attributes.datetime_last_access = kwargs.pop('datetime_last_access', None)
+    file_attributes.datetime_last_access = kwargs.pop('datetime_last_access',
+                                                      None)
     file_attributes.in_use = kwargs.pop('in_use', None)
     file_attributes.custom_attributes = kwargs.pop('custom_attributes', None)
 
     if kwargs:
-        raise AttributeError(kwargs.keys() + '  field(s) are not among attribute keys. Use custom attributes'
-                                             ' dict for saving it')
+        raise AttributeError(kwargs.keys() +
+                             '  field(s) are not among attribute keys. '
+                             ' Use custom attributes'
+                             ' dict for saving it')
     file_attributes.save(validate=True, write_concern={"w": 1})
 
     return file_attributes
@@ -102,9 +105,9 @@ def save_file_event_link(file_base, event_id,
 
     """
 
-    connect(db=database, host=host, port=port)
-
-    file_event_link = FileEventLink(file_base=file_base.id, event_id=event_id, link_parameters=link_parameters)
+    file_event_link = FileEventLink(file_base=file_base.id,
+                                    event_id=event_id,
+                                    link_parameters=link_parameters)
     file_event_link.save(validate=True, write_concern={"w": 1})
 
     return file_event_link
@@ -140,7 +143,9 @@ def find_file_event_link(file_base=None, event_id=None):
     elif event_id is not None:
         query_dict['event_id'] = event_id
     else:
-        raise AttributeError('Search parameters are invalid. file_base or event_id search is possible')
+        raise AttributeError(
+            'Search parameters are invalid. file_base '
+            'or event_id search is possible')
 
     return FileEventLink.objects(__raw__=query_dict)
 
@@ -152,13 +157,11 @@ def find_last():
     Returns
     --------
 
-    FileBase object
-    #I know i am violating numpy docs like a turkish in open buffet but have not looked into how to use it yet, until
-    #we decide what doc format to use, I will not bother, just write stuff we need for any format!
-
+    FileBase object #I know i am violating numpy docs like a turkish in
+    open buffet but have not looked into how to use it yet, until #we
+    decide what doc format to use, I will not bother, just write stuff
+    we need for any format!
     """
-
-    connect(db=database, host=host, port=port)
 
     result = FileBase.objects.order_by('-_id')[0:1][0]
     if result:
@@ -178,8 +181,6 @@ def find_file_attributes(file_base):
 
     """
 
-    connect(db=database, host=host, port=port)
-
     return FileAttributes.objects(file_base=file_base.id)
 
 
@@ -189,8 +190,10 @@ def find(properties=True, **kwargs):
     file_base_objects = find_file_base(**kwargs)
 
     for file_base_object in file_base_objects:
-        file_attribute_objects.append(find_file_attributes(file_base=file_base_object))
-        file_event_link_objects.append(find_file_event_link(file_base=file_base_object))
+        file_attribute_objects.append(
+            find_file_attributes(file_base=file_base_object))
+        file_event_link_objects.append(
+            find_file_event_link(file_base=file_base_object))
 
     return file_base_objects, file_attribute_objects, file_event_link_objects
 
