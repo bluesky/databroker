7e21635722b1fa1dfca467ab5fa32bc1d48100ec
danielballan <daniel.b.allan@gmail.com>
2015-02-12 11:37:15 -0500

    ENH: Read config file for db connection.

--- metadataStore/conf.py
+++ metadataStore/conf.py
@@ -1,7 +1,18 @@
-__author__ = 'arkilic'
+import os
+import yaml
+import logging
 
-mds_config = {
-    'database': 'metadataStore',
-    'host': "localhost",
-    'port': 27017,
-}
+logger = logging.getLogger(__name__)
+filename = os.path.join(os.path.expanduser('~'), '.config', 'metadatastore',
+                        'connection.yml')
+if os.path.isfile(filename):
+    with open(filename) as f:
+        mds_config = yaml.load(f)
+    logger.debug("Using db connection specified in config file.")
+else:
+    logger.debug("Using default db connection.")
+    mds_config = {
+        'database': 'metadataStore',
+        'host': "localhost",
+        'port': 27017,
+        }
