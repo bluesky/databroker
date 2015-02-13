1ca468d0a8fcbf2df9a92ad7ac0b02b53349703e
Thomas A Caswell <tcaswell@bnl.gov>
2015-02-13 15:16:33 -0500

    MNT : set default db -> test
    Also made logging a bit more verbose (include config)

--- metadataStore/conf.py
+++ metadataStore/conf.py
@@ -8,11 +8,13 @@ filename = os.path.join(os.path.expanduser('~'), '.config', 'metadatastore',
 if os.path.isfile(filename):
     with open(filename) as f:
         mds_config = yaml.load(f)
-    logger.debug("Using db connection specified in config file.")
+    logger.debug("Using db connection specified in config file. \n%r",
+                 mds_config)
 else:
-    logger.debug("Using default db connection.")
     mds_config = {
-        'database': 'metadataStore',
+        'database': 'test',
         'host': "localhost",
         'port': 27017,
         }
+    logger.debug("Using default db connection. \n%r",
+                 mds_config)
