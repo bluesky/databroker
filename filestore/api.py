4dfc575571ac6a28cd71e9831bb565c636fc0d95
Thomas A Caswell <tcaswell@bnl.gov>
2015-02-21 00:11:16 -0500

    API : merge all api into a single file

---
+++ filestore/api.py
@@ -0,0 +1,3 @@
+from __future__ import absolute_import, division, print_function
+
+from .commands import insert_resource, insert_nugget, retrieve_data
--- filestore/api/collection.py
+++
@@ -1,4 +0,0 @@
-__author__ = 'arkilic'
-
-from ..commands import (insert_resource, insert_resourse_attributes,
-                        insert_nugget)
