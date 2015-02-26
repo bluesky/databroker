ef6ce7961b122b56e161a1d731768366052bcdda
danielballan <daniel.b.allan@gmail.com>
2015-02-26 12:01:41 -0500

    API: Add db_disconnect to API.

--- filestore/api.py
+++ filestore/api.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import, division, print_function
 
-from .commands import insert_resource, insert_datum, retrieve
+from .commands import insert_resource, insert_datum, retrieve, db_disconnect
 from .retrieve import register_handler, deregister_handler
