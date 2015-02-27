789a180e635bb1239c19d43e1ec7412952af7ff9
Eric Dill <edill@bnl.gov>
2015-02-27 17:05:35 -0500

    MNT: Leaving unittests in place for now

--- dataportal/tests/test_muxer.py
+++ dataportal/tests/test_muxer.py
@@ -65,78 +65,78 @@ def test_timestamps_as_data():
 
 
 
-# class CommonBinningTests(object):
-#
-#     def test_bin_on_sparse(self):
-#         "Align a dense column to a sparse column."
-#
-#         # If downsampling is necessary but there is no rule, fail.
-#         bad_binning = lambda: self.dm.bin_on(self.sparse)
-#         self.assertRaises(BinningError, bad_binning)
-#
-#         result = self.dm.bin_on(self.sparse,
-#                                 agg={self.dense: self.agg})
-#         # With downsampling, the result should have as many entires as the
-#         # data source being "binned on" (aligned to).
-#         actual_len = len(result)
-#         expected_len = len(self.dm[self.sparse])
-#         self.assertEqual(actual_len, expected_len)
-#
-#         # There should be stats columns.
-#         self.assertTrue('max' in result[self.dense].columns)
-#
-#     def test_bin_on_dense(self):
-#         "Align a sparse column to a dense column"
-#
-#         # With or without upsampling, the result should have the sample
-#         # length as the dense column.
-#         expected_len = len(self.dm[self.dense])
-#         result1 = self.dm.bin_on(self.dense)
-#         actual_len1 = len(result1)
-#         self.assertEqual(actual_len1, expected_len)
-#         result2 = self.dm.bin_on(self.dense,
-#                                  interpolation={self.sparse: self.interp})
-#         actual_len2 = len(result2)
-#         self.assertEqual(actual_len2, expected_len)
-#
-#         # If there is an interpolation rule, there should be no missing values
-#         # except (perhaps) at the edges outside the domain of the sparse col.
-#         first = self.dm[self.sparse].first_valid_index()
-#         last = self.dm[self.sparse].last_valid_index()
-#         expected_len = 2 + len(self.dm[self.dense].loc[first:last])
-#         self.assertLess(result1[self.sparse]['val'].count(), expected_len)
-#         self.assertEqual(result2[self.sparse]['val'].count(), expected_len)
-#
-#         # There should not be stats columns.
-#         self.assertFalse('max' in result1[self.dense].columns)
-#
-#         # The dense column, being binned on, should have no 'count' column.
-#         self.assertFalse('count' in result1[self.dense].columns)
-#         # But the sparse column should.
-#         self.assertTrue('count' in result1[self.sparse].columns)
-#
-#
-# class TestBinningTwoScalarEvents(CommonBinningTests, unittest.TestCase):
-#
-#     def setUp(self):
-#         self.dm = DataMuxer.from_events(temperature_ramp.run())
-#         self.sparse = 'Tsam'
-#         self.dense = 'point_det'
-#         self.agg = np.mean
-#         self.interp = 'linear'
-#
-#
-# class TestBinningMultiSourceEvents(CommonBinningTests, unittest.TestCase):
-#
-#     def setUp(self):
-#         self.dm = DataMuxer.from_events(multisource_event.run())
-#         self.sparse = 'Troom'
-#         self.dense = 'point_det'
-#         self.agg = np.mean
-#         self.interp = 'linear'
-#
-#
-# class TestImageAndScalar(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.dm = DataMuxer.from_events(image_and_scalar.run())
+class CommonBinningTests(object):
+
+    def test_bin_on_sparse(self):
+        "Align a dense column to a sparse column."
+
+        # If downsampling is necessary but there is no rule, fail.
+        bad_binning = lambda: self.dm.bin_on(self.sparse)
+        self.assertRaises(BinningError, bad_binning)
+
+        result = self.dm.bin_on(self.sparse,
+                                agg={self.dense: self.agg})
+        # With downsampling, the result should have as many entires as the
+        # data source being "binned on" (aligned to).
+        actual_len = len(result)
+        expected_len = len(self.dm[self.sparse])
+        self.assertEqual(actual_len, expected_len)
+
+        # There should be stats columns.
+        self.assertTrue('max' in result[self.dense].columns)
+
+    def test_bin_on_dense(self):
+        "Align a sparse column to a dense column"
+
+        # With or without upsampling, the result should have the sample
+        # length as the dense column.
+        expected_len = len(self.dm[self.dense])
+        result1 = self.dm.bin_on(self.dense)
+        actual_len1 = len(result1)
+        self.assertEqual(actual_len1, expected_len)
+        result2 = self.dm.bin_on(self.dense,
+                                 interpolation={self.sparse: self.interp})
+        actual_len2 = len(result2)
+        self.assertEqual(actual_len2, expected_len)
+
+        # If there is an interpolation rule, there should be no missing values
+        # except (perhaps) at the edges outside the domain of the sparse col.
+        first = self.dm[self.sparse].first_valid_index()
+        last = self.dm[self.sparse].last_valid_index()
+        expected_len = 2 + len(self.dm[self.dense].loc[first:last])
+        self.assertLess(result1[self.sparse]['val'].count(), expected_len)
+        self.assertEqual(result2[self.sparse]['val'].count(), expected_len)
+
+        # There should not be stats columns.
+        self.assertFalse('max' in result1[self.dense].columns)
+
+        # The dense column, being binned on, should have no 'count' column.
+        self.assertFalse('count' in result1[self.dense].columns)
+        # But the sparse column should.
+        self.assertTrue('count' in result1[self.sparse].columns)
+
+
+class TestBinningTwoScalarEvents(CommonBinningTests, unittest.TestCase):
+
+    def setUp(self):
+        self.dm = DataMuxer.from_events(temperature_ramp.run())
+        self.sparse = 'Tsam'
+        self.dense = 'point_det'
+        self.agg = np.mean
+        self.interp = 'linear'
+
+
+class TestBinningMultiSourceEvents(CommonBinningTests, unittest.TestCase):
+
+    def setUp(self):
+        self.dm = DataMuxer.from_events(multisource_event.run())
+        self.sparse = 'Troom'
+        self.dense = 'point_det'
+        self.agg = np.mean
+        self.interp = 'linear'
+
+
+class TestImageAndScalar(unittest.TestCase):
+
+    def setUp(self):
+        self.dm = DataMuxer.from_events(image_and_scalar.run())
