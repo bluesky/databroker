90a5f06877b6122554a2a0b1dec11a054f27c2f2
Arman Arkilic <arkilic@bnl.gov>
2015-03-02 09:09:54 -0500

    WIP:Got rid off fetch_events in api.py

--- metadatastore/api.py
+++ metadatastore/api.py
@@ -1,7 +1,7 @@
 # Data retrieval
 from .commands import (find, find_event, find_event_descriptor,
                        find_beamline_config, find_run_start, find_last,
-                       find_event_given_descriptor, fetch_events, find_run_stop)
+                       find_event_given_descriptor, find_run_stop)
 # Data insertion
 from .commands import (insert_event, insert_event_descriptor, insert_run_start,
                        insert_beamline_config, insert_run_stop,
