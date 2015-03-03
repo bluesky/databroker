9dc6b23c41e4f0c93df63fffc2fe638587c1a2ea
licode <lili@bnl.gov>
2015-03-03 11:46:47 -0500

    TST: add setup and teardown

--- dataportal/tests/test_example_hdf_io.py
+++ dataportal/tests/test_example_hdf_io.py
@@ -2,6 +2,18 @@ import numpy as np
 from nose.tools import assert_equal, assert_not_equal
 from filestore.api import retrieve
 from ..examples.hdf_io import hdf_data_io
+from metadatastore.utils.testing import mds_setup, mds_teardown
+from filestore.utils.testing import fs_setup, fs_teardown
+
+
+def setup():
+    fs_setup()
+    mds_setup()
+
+
+def teardown():
+    fs_teardown()
+    mds_teardown()
 
 
 def _test_retrieve_data(evt):
