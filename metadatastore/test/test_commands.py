c52d7d971ef24ee5c80cd082f6348b9c122baf1f
Eric Dill <edill@bnl.gov>
2015-03-07 10:36:37 -0500

    TST: insert an event with a None ev_desc

--- metadatastore/test/test_commands.py
+++ metadatastore/test/test_commands.py
@@ -8,11 +8,12 @@ import time as ttime
 from ..api import Document as Document
 
 from nose.tools import make_decorator
-from nose.tools import assert_equal, assert_raises
+from nose.tools import assert_equal, assert_raises, raises
 
 
 from metadatastore.odm_templates import (BeamlineConfig, EventDescriptor,
                                          Event, RunStart, RunStop)
+from metadatastore.commands import EventDescriptorIsNoneError
 import metadatastore.commands as mdsc
 from metadatastore.utils.testing import mds_setup, mds_teardown
 from metadatastore.examples.sample_data import temperature_ramp
@@ -100,6 +101,10 @@ def test_ev_desc():
                                'external': 'FS:foobar'}}
     time = ttime.time()
     yield _ev_desc_tester, rs, data_keys, time
+
+@raises(EventDescriptorIsNoneError)
+def test_ev_insert_fail():
+    ev = mdsc.insert_event(None, ttime.time(), data={'key': [0, 0]}, seq_num=0)
 
 
 def test_dict_key_replace_rt():
