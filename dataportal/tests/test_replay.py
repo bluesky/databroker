0d711aad4213d48c1c46cc9b1ac0d9168c2550fb
Eric Dill <edill@bnl.gov>
2015-03-09 21:41:35 -0400

    ENH: Adding xvfb to travis for Gui testing

--- dataportal/tests/test_replay.py
+++ dataportal/tests/test_replay.py
@@ -1,19 +1,23 @@
-import enaml
-from enaml.qt.qt_application import QtApplication
-import sys
+import six
+if six.PY2:
+    # hide enaml imports from python 3
+    import enaml
+    from enaml.qt.qt_application import QtApplication
+    from dataportal.replay import replay
+
 from metadatastore.utils.testing import mds_setup, mds_teardown
 from filestore.utils.testing import fs_setup, fs_teardown
 from dataportal.examples.sample_data import temperature_ramp, image_and_scalar
 from dataportal.broker import DataBroker as db
-from dataportal.replay import replay
 import copy
-
+from ..testing.decorators import skip_if
 
 global hdr_temp_ramp, ev_temp_ramp
 global hdr_img_scalar, ev_img_scalar
 global app
 
 
+@skip_if(not six.PY2)
 def setup():
     mds_setup()
     fs_setup()
@@ -30,17 +34,18 @@ def setup():
     app = QtApplication()
 
 
+@skip_if(not six.PY2)
 def teardown():
     fs_teardown()
     mds_teardown()
 
-
+@skip_if(not six.PY2)
 def _replay_startup_tester(params=None, wait_time=1000):
     replay.create_and_show(params)
     app.timed_call(wait_time, app.stop)
     app.start()
 
-
+@skip_if(not six.PY2)
 def test_replay_startup():
     normal = replay.define_default_params()
     normal_small = copy.deepcopy(normal)
