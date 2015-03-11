c0d96439f524d7b006f01f62e6b665bf3bd5087c
Thomas A Caswell <tcaswell@bnl.gov>
2015-03-11 10:05:44 -0400

    TST : test expected failure modes

--- dataportal/tests/test_replay_persistence.py
+++ dataportal/tests/test_replay_persistence.py
@@ -1,7 +1,7 @@
 from nose.tools import assert_equal
 from dataportal.replay.persist import History
-import dataportal.replay.persist
 
+from nose.tools import assert_raises
 OBJ_ID_LEN = 36
 h = None
 
@@ -27,3 +27,11 @@ def test_history():
     # And get(..., 1) returns previous.
     result1 = h.get(run_id, 1)
     assert_equal(result1, config1)
+
+
+def test_trim():
+    assert_raises(NotImplementedError, h.trim)
+
+
+def test_neg_numback_fails():
+    assert_raises(ValueError, h.get, 'test', -1)
