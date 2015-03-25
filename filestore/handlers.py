7115662996a03d865813d3d8d6761255d170358e
XPD Operator <xf28id1@xf28id1-ws2.cs.nsls2.local>
2015-03-25 15:33:54 -0400

    FIX: Raise error if frame no exceed number of frames found.

--- filestore/handlers.py
+++ filestore/handlers.py
@@ -61,6 +61,9 @@ class AreaDetectorTiffHandler(HandlerBase):
 
     def __call__(self, point_number):
         start, stop = point_number * self._fpp, (point_number + 1) * self._fpp
+        if stop > len(self._image_sequence):
+            raise IntegrityError("Seeking Frame {0} out of {1} frames.".format(
+                stop, len(self._image_sequence)))
         return np.asarray(list(self._image_sequence[start:stop])).squeeze()
 
 
