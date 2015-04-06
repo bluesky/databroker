2eb7a5bb68fecd2b879bb3dba9d9fc5bed932e7d
Eric Dill <edill@bnl.gov>
2015-04-06 16:56:38 -0400

    MNT: Protect repo from enaml vs python 3

--- dataportal/__init__.py
+++ dataportal/__init__.py
@@ -5,8 +5,14 @@ from .sources import *
 
 logger = logging.getLogger(__name__)
 __version__ = 'v0.0.5.post0'
+
 # import qt from enaml to make sure that the Qt API version gets set correctly
-from enaml import qt
+try:
+    from enaml import qt
+except ImportError:
+    pass
+else:
+   del qt
 
 # generally useful imports
 from .broker import DataBroker
