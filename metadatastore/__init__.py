eb3a3f563364df406bb85b0e11865532dcb00308
Thomas A Caswell <tcaswell@bnl.gov>
2015-04-06 16:48:10 -0400

    REL : version bump after release

--- metadatastore/__init__.py
+++ metadatastore/__init__.py
@@ -1,3 +1,3 @@
 from . import conf, commands, odm_templates
 
-__version__ = 'v0.0.5'
+__version__ = 'v0.0.5.post0'
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import os
 MAJOR = 0
 MINOR = 5
 MICRO = 0
-ISRELEASED = True
+ISRELEASED = False
 SNAPSHOT = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 QUALIFIER = ''
