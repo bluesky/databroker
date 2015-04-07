2b165fd71f57990eaf763cd1b45e155295363758
Thomas A Caswell <tcaswell@bnl.gov>
2015-04-07 09:55:05 -0400

    REL v0.0.6
    New Fetures
    ===========
    
    Broker
    ------
    
    - StepScan interface to return stepscan data as dataframe in one step
    - Search for headers by detector/motor alias
    - Seach for headers by partial uid
    
    Muxer
    -----
    
    - DataMuxer now has a `plan` accessor to provide information about
      exactly what a `bin_on_*` operation will do
    - Add `DataMWuxer.to_sparse_dataframe` to return data with one event
      per row.
    
    replay
    ------
    
    - plotting configuration presists on changing scans and between
      sessions.
    
    API changes
    ===========
    
    - `fetch_events` returns a generator instead of a list to defer I/O

--- dataportal/__init__.py
+++ dataportal/__init__.py
@@ -4,7 +4,7 @@ from .sources import *
 
 
 logger = logging.getLogger(__name__)
-__version__ = 'v0.0.5.post0'
+__version__ = 'v0.0.6'
 
 # import qt from enaml to make sure that the Qt API version gets set correctly
 try:
