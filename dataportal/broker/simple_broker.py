c468abf6b5077610869eaee9f126d8b2b699f752
Thomas A Caswell <tcaswell@bnl.gov>
2015-04-09 17:11:54 -0400

    ENH : make sure print prints everything

--- dataportal/broker/simple_broker.py
+++ dataportal/broker/simple_broker.py
@@ -404,14 +404,17 @@ class Header(Document):
                 self.scan_id, self.run_start_uid)
 
     def __str__(self):
+        special_keys = set(('owner', 'group', 'project', 'beamline_id',
+                            'scan_id', 'start_datetime', 'stop_datetime',
+                            'exit_status', 'exit_reason', 'run_start_uid',
+                            'run_stop_uid', ))
         s = Stream()
         s.write("<Header>")
         s.write("Owner: {0}".format(self.owner))
-        if self.group is not None:
+        if self.group:
             s.write("Group: {0}".format(self.group))
-        if self.project is not None:
+        if self.project:
             s.write("Project: {0}".format(self.project))
-        s.write("Project: {0}".format(self.project))
         s.write("Beamline ID: {0}".format(self.beamline_id), 'lightgray')
         s.write("Scan ID: {0} ".format(self.scan_id), 'green')
         s.write("Start Time: {0}".format(self.start_datetime), 'green')
@@ -431,6 +434,11 @@ class Header(Document):
         if hasattr(self, 'run_stop_uid'):
             s.write("run_stop_uid='{0}'".format(self.run_stop_uid),
                     'lightgrey')
+        for k in sorted(self.keys()):
+            if k in special_keys:
+                continue
+            s.write("{0}: {1}".format(k, self[k]))
+
         return s.readout()
 
 
