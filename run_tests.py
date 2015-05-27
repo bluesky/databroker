d27723ed3c3006a5bd9567e992c6d37a9ca0d159
danielballan <daniel.b.allan@gmail.com>
2015-05-27 16:06:49 -0400

    MNT: Remove coverage on replay; development is suspended.

--- run_tests.py
+++ run_tests.py
@@ -20,7 +20,7 @@ from dataportal.testing.noseclasses import KnownFailure
 
 plugins = [KnownFailure]
 env = {"NOSE_WITH_COVERAGE": 1,
-       'NOSE_COVER_PACKAGE': ['dataportal', 'replay'],
+       'NOSE_COVER_PACKAGE': ['dataportal'],
        'NOSE_COVER_HTML': 1}
 # Nose doesn't automatically instantiate all of the plugins in the
 # child processes, so we have to provide the multiprocess plugin with
