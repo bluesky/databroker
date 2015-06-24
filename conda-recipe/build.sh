7d97a7fb2404fdfbd784a1b049b76bd15d7d1e1c
Thomas A Caswell <tcaswell@bnl.gov>
2015-06-24 15:21:40 -0400

    BLD: conda magic to find ENV/etc
    Due to constraints from the jupyterhub tool chain we can not activate
    enviroments which means we can not find the configuration.  This uses
    conda's path re-writing logic to insert a hard-coded path into the
    installed files to point to the right path.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sed -i 's|__CONDA_BUILD_PLACEHOLDER__|'$PREFIX'/etc|' filestore/conf.py
+
 $PYTHON setup.py build
-$PYTHON setup.py install
+$PYTHON setup.py install --single-version-externally-managed --record=/dev/null
