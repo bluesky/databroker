59ca8de7d68bf021f44fe110feffa8312d9e5d33
Thomas A Caswell <tcaswell@bnl.gov>
2015-07-16 17:51:05 -0400

    ENH: warn if more datum than cache space
    If the cache is smaller than the number of datum in a single resource
    the likely hood of expensive cache misses is high.

--- filestore/retrieve.py
+++ filestore/retrieve.py
@@ -248,10 +248,15 @@ def get_data(eid, handle_registry=None):
         datum = {k: edoc[k] for k in keys}
         # pull all datum which share this resource
         res = edoc['resource']
+        count = 0
         for dd in d_objs(resource=res):
+            count += 1
             d_id = dd['datum_id']
             if d_id not in _DATUM_CACHE:
                 _DATUM_CACHE[d_id] = {k: dd[k] for k in keys}
+        if count > _DATUM_CACHE.max_size:
+            logger.warn("More datum in a resource than your "
+                        "datum cache can hold.")
 
     handler = get_spec_handler(datum['resource'], handle_registry)
     return handler(**datum['datum_kwargs'])
