01271586569f6988dfc2e7c9fd2f17637029cefc
Thomas A Caswell <tcaswell@bnl.gov>
2015-07-22 17:16:00 -0400

    ENH: Greatly simplify and make work

--- filestore/path_only_handlers.py
+++ filestore/path_only_handlers.py
@@ -5,25 +5,19 @@ import six
 import logging
 
 from .retrieve import HandlerBase
-from .handlers import IntegrityError
 
 logger = logging.getLogger(__name__)
 
 
-class AreaDetectorTiffHandlerPath(HandlerBase):
-    specs = {'AD_TIFF'} | HandlerBase.specs
+class AreaDetectorTiffPathOnlyHandler(HandlerBase):
 
     def __init__(self, fpath, template, filename, frame_per_point=1):
         self._path = fpath
         self._fpp = frame_per_point
-        self._template = template.replace('_%6.6d', '_{frm:06d}')
-        self._filename = self._template % (self._path, filename)
+        self._template = template
+        self._filename = filename
 
     def __call__(self, point_number):
         start, stop = point_number * self._fpp, (point_number + 1) * self._fpp
-        if stop > len(self._image_sequence):
-            raise IntegrityError("Seeking Frame {0} out of {1} frames.".format(
-                stop, len(self._image_sequence)))
-        slc = slice(start, stop)
-        return list(self._filename.format(frm=frm)
-                    for frm in range(*slc.indices(stop + 1)))
+        return [self._template % (self._path, self._filename, n)
+                for n in range(start, stop)]
