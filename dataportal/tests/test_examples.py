50a97734c87aeeb18801585b345283a942ba994a
Thomas A Caswell <tcaswell@bnl.gov>
2015-09-03 00:01:18 -0400

    TST: fix imports in test_examples

--- dataportal/tests/test_examples.py
+++ dataportal/tests/test_examples.py
@@ -1,14 +1,15 @@
 from nose.tools import assert_true
 from ..examples.sample_data import (temperature_ramp, multisource_event,
                                     image_and_scalar, step_scan)
-from ..broker.document import Document
+import six
 from filestore.utils.testing import fs_setup, fs_teardown
 from metadatastore.utils.testing import mds_setup, mds_teardown
-import six
+
 from ..testing.utils import Command
 
 
 examples = [temperature_ramp, multisource_event, image_and_scalar, step_scan]
+
 
 def setup():
     fs_setup()
@@ -29,6 +30,7 @@ def run_example_programmatically(example):
 def run_example_cmdline(example):
     command = Command('python {}'.format(example.__file__))
     command.run(timeout=1)
+
 
 def test_examples_programmatically():
     for example in examples:
