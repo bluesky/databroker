58655dae15dddc64a2e236e0d84be659b9f71c07
Thomas A Caswell <tcaswell@bnl.gov>
2015-09-09 16:54:41 -0400

    STY: clean up whitespace in api.py

--- metadatastore/api.py
+++ metadatastore/api.py
@@ -1,8 +1,6 @@
 # Data retrieval
-from .commands import (find_events, find_descriptors,
-                       find_run_starts, find_last,
-                       find_run_stops)
+from .commands import (find_events, find_descriptors, find_run_starts,
+                       find_last, find_run_stops)
 # Data insertion
 from .commands import (insert_event, insert_descriptor, insert_run_start,
-                       insert_run_stop,
-                       db_connect, db_disconnect)
+                       insert_run_stop, db_connect, db_disconnect)
