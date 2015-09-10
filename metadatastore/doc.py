9b5b09969cac83fe43fce055a9d9f971f3ec294a
Thomas A Caswell <tcaswell@bnl.gov>
2015-09-09 23:32:31 -0400

    MNT/STY: minor changes

--- metadatastore/doc.py
+++ metadatastore/doc.py
@@ -1,5 +1,4 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import (absolute_import, division, print_function)
 import six
 import collections
 from functools import reduce
@@ -106,8 +105,6 @@ class Document(dict):
 
         ret : dict
             Data payload of Document
-
-
         """
         ret = dict(self)
         name = ret.pop('_name')
