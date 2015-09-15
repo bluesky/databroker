b77c7e00b01aaa3c52fab34c6271e59a7a7aa19f
Eric Dill <edill@bnl.gov>
2015-09-15 17:58:58 -0400

    PEP8

--- metadatastore/test/test_commands.py
+++ metadatastore/test/test_commands.py
@@ -191,9 +191,11 @@ def test_find_events_smoke():
 
     next(mdsc.find_events())
 
+
 @raises(ValueError)
 def test_find_events_ValueError():
     list(mdsc.find_events(event_descriptor='cat'))
+
 
 @raises(ValueError)
 def test_bad_bulk_insert_event_data():
