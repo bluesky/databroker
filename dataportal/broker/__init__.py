21b292b4474724777d431431b8f8448fefb48e9b
danielballan <daniel.b.allan@gmail.com>
2015-09-15 12:53:42 -0400

    API: Add new stuff; remove dead stuff.

--- dataportal/broker/__init__.py
+++ dataportal/broker/__init__.py
@@ -1,4 +1,4 @@
-from .simple_broker import (DataBroker, Header, IntegrityError, fill_event)
+from .simple_broker import (DataBroker, Header, get_events, get_table)
 from .handler_registration import register_builtin_handlers
 from .pims_readers import Images, SubtractedImages
 
