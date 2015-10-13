6e64e65eaa7ecd90ce46927cc98f581ceb3d46dc
arkilic <arkilic@bnl.gov>
2015-10-13 15:44:03 -0400

    WIP:Separated startup main thread from handlers

--- metadataservice/server/engine.py
+++ metadataservice/server/engine.py
@@ -363,13 +363,3 @@ class CappedRunStopHandler(tornado.web.RequestHandler):
         raise tornado.web.HTTPError(404)
 
 
-db = db_connect('datastore2', '127.0.0.1', 27017)
-loop = tornado.ioloop.IOLoop.instance()
-application = tornado.web.Application([
-    (r'/run_start', RunStartHandler), (r'/run_stop', RunStopHandler),
-    (r'/event_descriptor',EventDescriptorHandler),
-    (r'/event',EventHandler),
-    (r'/run_start_capped', CappedRunStartHandler), 
-    (r'/run_stop_capped', CappedRunStopHandler)], db=db)
-application.listen(7770)
-loop.start()
--- metadataservice/server/test/test_api.py
+++ metadataservice/server/test/test_api.py
@@ -1,16 +1,7 @@
 # Smoketest the api
 
-from metadataservice.client.api import find_events
-from metadataservice.client.api import find_descriptors
-from metadataservice.client.api import find_run_starts
-from metadataservice.client.api import find_run_stops
-from metadataservice.client.api import find_last
-from metadataservice.client.api import insert_event
-from metadataservice.client.api import insert_descriptor
-from metadataservice.client.api import insert_run_start
-from metadataservice.client.api import insert_run_stop
-from metadataservice.client.api import server_connect
-# from metadataservice.client.api import db_disconnect
+
+
 
 
 if __name__ == "__main__":
