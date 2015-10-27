01cbfba72a9a78a1e172acca2e1f59af1d219b5d
arkilic <arkilic@bnl.gov>
2015-10-27 11:44:51 -0400

    ENH: Server startup is now guaranteed to be in the main thread

--- metadataservice/server/startup.py
+++ metadataservice/server/startup.py
@@ -8,11 +8,14 @@ from metadataservice.server.engine import (db_connect, RunStartHandler, RunStopH
 
 db = db_connect('datastore2', '127.0.0.1', 27017)
 
-application = tornado.web.Application([
-    (r'/run_start', RunStartHandler), (r'/run_stop', RunStopHandler),
-    (r'/event_descriptor',EventDescriptorHandler),
-    (r'/event',EventHandler),
-    (r'/run_start_capped', CappedRunStartHandler),
-    (r'/run_stop_capped', CappedRunStopHandler)], db=db)
-application.listen(7770)
-loop.start()
+
+
+if __name__ == "__main__":
+    application = tornado.web.Application([
+        (r'/run_start', RunStartHandler), (r'/run_stop', RunStopHandler),
+        (r'/event_descriptor',EventDescriptorHandler),
+        (r'/event',EventHandler),
+        (r'/run_start_capped', CappedRunStartHandler),
+        (r'/run_stop_capped', CappedRunStopHandler)], db=db)
+    application.listen(7770)
+    loop.start()
