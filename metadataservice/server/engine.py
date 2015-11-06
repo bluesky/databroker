cc83772c2dc5466ab1d7b9c7a96ec240581d0154
arkilic <arkilic@bnl.gov>
2015-11-06 10:08:01 -0500

    WIP:No more pagination on descriptor retrieval

--- metadataservice/server/engine.py
+++ metadataservice/server/engine.py
@@ -124,15 +124,9 @@ class EventDescriptorHandler(tornado.web.RequestHandler):
     def get(self):
         database = self.settings['db']
         query = utils._unpack_params(self)
-        try:
-            start = query.pop('range_floor')
-            stop = query.pop('range_ceil')
-        except KeyError:
-            raise tornado.web.HTTPError(400,
-                                        "range_ceil and range_floor required parameters")
         docs = yield database.event_descriptor.find(query).sort(
-            'time', pymongo.ASCENDING)[start:stop].to_list(None)
-        if not docs and start == 0:
+            'time', pymongo.ASCENDING).to_list(None)
+        if not docs:
             raise tornado.web.HTTPError(500, 
                                         reason='No results found for query')
         else:
