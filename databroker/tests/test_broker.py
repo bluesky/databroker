02fb8d4404b0191c7064112df818722a5acb8735
Eric Dill <edill@bnl.gov>
2015-11-19 10:16:03 -0500

    MNT: Don't import unicode literals
    It causes nothing but trouble...

--- databroker/tests/test_broker.py
+++ databroker/tests/test_broker.py
@@ -1,6 +1,4 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
-from collections import deque
+from __future__ import absolute_import, division, print_function
 import six
 import uuid
 import logging
