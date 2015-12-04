13d116d0424fe9ec1a4d43b0776d4f16b2a78cc4
Eric Dill <edill@bnl.gov>
2015-12-04 14:43:49 -0500

    MNT: Rework setuptools to by consistent
    Reworking setuptools to be consistent with the python packaging
    guidelines

--- setup.py
+++ setup.py
@@ -1,25 +1,39 @@
-import sys
-import warnings
 import versioneer
+from setuptools import setup, find_packages
 
+# To use a consistent encoding
+from codecs import open
+from os import path
 
-try:
-    from setuptools import setup
-except ImportError:
-    try:
-        from setuptools.core import setup
-    except ImportError:
-        from distutils.core import setup
+here = path.abspath(path.dirname(__file__))
 
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='databroker',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
+    # Author details
     author='Brookhaven National Laboratory',
-    packages=['databroker', 'databroker.testing',
-              'databroker.examples',
-              'databroker.examples.sample_data',
-              'databroker.utils',
-              ],
+
+    packages=find_packages(),
+    description='Unification of NSLS-II data sources',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/NSLS-II/databroker',
+
+    license='BSD (3-clause)',
+
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
 )
