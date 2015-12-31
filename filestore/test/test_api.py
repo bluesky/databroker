16a5f6d3338e197a7b0cef5513e271737687c1f1
Thomas A Caswell <tcaswell@bnl.gov>
2015-12-31 00:43:45 -0500

    TST: add import warning test

--- filestore/test/test_api.py
+++ filestore/test/test_api.py
@@ -1,7 +1,18 @@
 import filestore
+import warnings
 
 
 def test_top_level_imports():
     filestore.FileStore
     filestore.FileStoreRO
     filestore.DatumNotFound
+
+
+def test_import_warnings():
+    with warnings.catch_warnings(record=True) as w:
+        # Cause all warnings to always be triggered.
+        warnings.simplefilter("always")
+        # Trigger a warning.
+        import filestore.commands
+        import filestore.retrieve
+        assert len(w) == 2
