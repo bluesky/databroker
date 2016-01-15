b792b2d5e6a4fb1004c119da8ed41189c9438150
Thomas A Caswell <tcaswell@bnl.gov>
2016-01-15 15:54:44 -0500

    ENH: add argparsing to pass db name to startup.py

--- startup.py
+++ startup.py
@@ -1,18 +1,32 @@
 """ Startup script for the server."""
+import argparse
 #TODO: Replace this with a better startup mechanism
 import tornado.web
 from metadataservice.server.engine import (RunStartHandler, RunStopHandler,
                                            EventDescriptorHandler, EventHandler,
                                            # CappedRunStartHandler, CappedRunStopHandler,
                                            loop, db_connect)
-from metadataservice.server.conf import connection_config
+from metadataservice.server.conf import load_configuration
 
-
-db = db_connect(connection_config['database'],
-                connection_config['host'],
-                connection_config['port'])
 
 if __name__ == "__main__":
+
+    config = {k: v for k, v in load_configuration('metadataservice', 'MDS',
+                                                  ['host', 'port', 'timezone'],
+                                                  allow_missing=True).items()
+              if v is not None}
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--database', dest='database', type=str,
+                        help='name of database to use')
+    args = parser.parse_args()
+    if args.database is not None:
+        config['database'] = args.database
+
+    db = db_connect(config['database'],
+                    config['host'],
+                    config['port'])
+    print(db)
     application = tornado.web.Application([
         (r'/run_start', RunStartHandler), (r'/run_stop', RunStopHandler),
         (r'/event_descriptor', EventDescriptorHandler),
