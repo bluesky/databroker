6c9f9864870eedc3339db1721de7ab778759f8e4
Thomas A Caswell <tcaswell@bnl.gov>
2016-02-05 18:38:08 -0500

    TST: make hdf5 tests use databroker

--- databroker/tests/test_example_hdf_io.py
+++ databroker/tests/test_example_hdf_io.py
@@ -1,9 +1,10 @@
-import numpy as np
-from nose.tools import assert_equal, assert_not_equal
-from filestore.api import retrieve
+
+from databroker import DataBroker as db, get_events
+
 from databroker.examples.hdf_io import hdf_data_io
-from metadataclient.testing_utils import mds_setup, mds_teardown
+from metadatastore.utils.testing import mds_setup, mds_teardown
 from filestore.utils.testing import fs_setup, fs_teardown
+from numpy.testing.utils import assert_array_equal
 
 
 def setup():
@@ -16,14 +17,18 @@ def teardown():
     mds_teardown()
 
 
-def _test_retrieve_data(event):
-    uid = event['data']['xrf_spectrum']
-    data = retrieve(uid)
-    assert_equal(data.size, 20)
-    assert_not_equal(np.sum(data), 0)
+def _test_retrieve_data(event, rows, cols):
+    spec = event['data']['xrf_spectrum']
+    x = event['data']['h_pos']
+    y = event['data']['v_pos']
+    assert spec.size == 20
+    assert_array_equal(spec, y * cols + x)
 
 
 def test_hdf_io():
-    events = hdf_data_io()
-    for e in events:
-        yield _test_retrieve_data, e
+    rows, cols = 1, 5
+    rs_uid, ev_uids = hdf_data_io(rows, cols)
+    h = db[rs_uid]
+    for e in get_events(h):
+        yield _test_retrieve_data, e, rows, cols
+        assert e['uid'] in ev_uids
