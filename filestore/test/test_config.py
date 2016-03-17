2fe0628eb40d8229fecd2c744bad23e267e485e9
Eric Dill <edill@bnl.gov>
2016-03-17 17:22:09 -0400

    test_config is a little tricky. Will revisit

--- filestore/test/test_config.py
+++ filestore/test/test_config.py
@@ -7,34 +7,34 @@ import filestore.conf as fconf
 import os
 import yaml
 import tempfile
-
-from nose.tools import assert_equal
+import pytest
 
 
 class test_read_connection_config_file(object):
-    # example contents of connection.yml
-    demo_conf = {'database': 'aardvark',
-                 'host': 'shadowbroker',
-                 'port': 94}
-
-    def setup(self):
+    @classmethod
+    def setup(cls):
+        # example contents of connection.yml
+        cls.demo_conf = {'database': 'aardvark',
+                         'host': 'shadowbroker',
+                         'port': 94}
         # stash the existing configuration
-        self._old_conf = fconf.connection_config
+        cls._old_conf = fconf.connection_config
         # make a yaml file wit the test data
         with tempfile.NamedTemporaryFile(delete=False) as fout:
-            self._fname = fout.name
-            print(self.demo_conf)
-            fout.write(yaml.dump(self.demo_conf, encoding='utf-8'))
+            cls._fname = fout.name
+            print(cls.demo_conf)
+            fout.write(yaml.dump(cls.demo_conf, encoding='utf-8'))
 
-    def teardown(self):
+    @classmethod
+    def teardown(cls):
         # remove the test config file
-        os.unlink(self._fname)
+        os.unlink(cls._fname)
         # restore the connection configuration...testing globals is fun!
-        fconf.connection_config = self._old_conf
+        fconf.connection_config = cls._old_conf
 
     def test_read_config_file(self):
         fconf.connection_config = fconf.load_configuration('filestore', 'FS',
                                      ['host', 'database', 'port'],
                                      fname=self._fname)
 
-        assert_equal(self.demo_conf, fconf.connection_config)
+        assert self.demo_conf == fconf.connection_config
