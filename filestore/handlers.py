a4395adb09faf39c470a67ff601e2f1d2cc264b7
Stuart B. Wilkins <swilkins@bnl.gov>
2016-05-07 13:11:40 -0400

    ENH: Added AD timestamp handlers

--- filestore/handlers.py
+++ filestore/handlers.py
@@ -187,6 +187,73 @@ class AreaDetectorHDF5SWMRHandler(AreaDetectorHDF5Handler):
         self._file = h5py.File(self._filename, 'r', swmr=True)
 
 
+class AreaDetectorHDF5TimestampHandler(HandlerBase):
+    """ Handler to retrieve timestamps from Areadetector HDF5 File
+
+    In this spec, the timestamps of the images are read.
+
+    Parameters
+    ----------
+    filename : string
+        path to HDF5 file
+    frame_per_point : integer, optional
+        number of frames to return as one datum, default 1
+    """
+    specs = {'AD_HDF5_TS'} | HandlerBase.specs
+
+    def __init__(self, filename, frame_per_point=1):
+        self._fpp = frame_per_point
+        self._filename = filename
+        self._key = ['/entry/instrument/NDAttributes/NDArrayEpicsTSSec',
+                     '/entry/instrument/NDAttributes/NDArrayEpicsTSnSec']
+        self._file = None
+        self._dataset1 = None
+        self._dataset2 = None
+        self.open()
+
+    def __call__(self, point_number):
+        # Don't read out the dataset until it is requested for the first time.
+        if not self._dataset1:
+            self._dataset1 = self._file[self._key[0]]
+        if not self._dataset2:
+            self._dataset2 = self._file[self._key[1]]
+        start, stop = point_number * self._fpp, (point_number + 1) * self._fpp
+        rtn = self._dataset1[start:stop].squeeze()
+        rtn = rtn + (self._dataset2[start:stop].squeeze() * 1e-9)
+        return rtn
+
+    def open(self):
+        if self._file:
+            return
+        self._file = h5py.File(self._filename, 'r')
+
+    def close(self):
+        super(AreaDetectorHDF5TimestampHandler, self).close()
+        self._file.close()
+        self._file = None
+
+
+class AreaDetectorHDF5SWMRTimestampHandler(AreaDetectorHDF5TimestampHandler):
+    """ Handler to retrieve timestamps from Areadetector HDF5 File
+
+    In this spec, the timestamps of the images are read. Reading
+    is done using SWMR option to allow read during processing
+
+    Parameters
+    ----------
+    filename : string
+        path to HDF5 file
+    frame_per_point : integer, optional
+        number of frames to return as one datum, default 1
+    """
+    specs = {'AD_HDF5_SWMR_TS'} | HandlerBase.specs
+
+    def open(self):
+        if self._file:
+            return
+        self._file = h5py.File(self._filename, 'r', swmr=True)
+
+
 class _HdfMapsHandlerBase(HDF5DatasetSliceHandler):
     """
     Reader for XRF data stored in hdf5 files.
