e0ce54206cd0e05dc0f2d62566b647e4ede70e93
arkilic <armanarkilic@gmail.com>
2016-06-01 13:05:50 -0400

    TST:Add sleep() to ensure start and end of server process

--- metadataservice/server/test/test_urls.py
+++ metadataservice/server/test/test_urls.py
@@ -1,2 +1,10 @@
 import requests
+from .utils import mds_setup, mds_teardown
 
+
+def setup():
+    mds_setup()
+
+
+def teardown():
+    mds_teardown()
--- metadataservice/server/test/utils.py
+++ metadataservice/server/test/utils.py
@@ -1,7 +1,7 @@
 import uuid
 import requests
 import uuid
-
+import time as ttime
 
 testing_config = dict(mongohost='localhost', mongoport=27017,
                       database='mds_test'+str(uuid.uuid4()),
@@ -13,7 +13,9 @@ def mds_setup():
     proc = Popen(["python", "startup.py", "--mongohost", "localhost", "--mongoport", 
            "27017", "--database", "mds_test", "--tzone", "timezone", "--serviceport", 
            "8899"])
-
+    ttime.sleep(1) # make sure the process is started
+    # TODO: Drop the database created using pymongo here!
 
 def mds_teardown():
     proc2 = Popen(['kill', '-9', str(proc.pid)])
+    ttime.sleep(1) # make sure the process is killed
