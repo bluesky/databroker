dd09582bc378072a3ae0acc3647ac545cbf68e6f
arkilic <armanarkilic@gmail.com>
2016-06-02 10:32:37 -0400

    TST/ENH:An improved test infrastructure and some new tests

--- metadataservice/test/test_urls.py
+++ metadataservice/test/test_urls.py
@@ -1,26 +1,37 @@
 import requests
 from .utils import mds_setup, mds_teardown, testing_config
 import ujson
-from time import sleep
+import uuid
+from time import sleep, time
 
-def setup():
-    mds_setup()
+class TestUrls:
+    def setup_class(self):
+        mds_setup()
+        self.url_dict = {'run_start': 'find_run_starts',
+                          'run_stop': 'find_run_stops',
+                          'event': 'find_events',
+                          'event_descriptor': 'find_descriptors'}
+        self.host='localhost'
+        self.port=testing_config['serviceport']
+        self.base_url = 'http://{}:{}/'.format(self.host, self.port)
 
-def test_smoke_urls():
-    host='localhost'
-    port=testing_config['serviceport']
-    test_dict = {'run_start': 'find_run_starts',
-                 'run_stop': 'find_run_stops',
-                 'event': 'find_events',
-                 'event_descriptor': 'find_descriptors'}
-    base_url = 'http://{}:{}/'.format(host, port)
-    trans_dict = dict(query={}, signature='find_run_start')
-    for k, v in test_dict.items():
-        url = base_url + k
-        message = dict(query={}, signature=v)
-        r = requests.get(url, params=ujson.dumps(message))
+    def test_smoke_urls(self):
+        trans_dict = dict(query={}, signature='find_run_start')
+        for k, v in self.url_dict.items():
+            url = self.base_url + k
+            message = dict(query={}, signature=v)
+            r = requests.get(url, params=ujson.dumps(message))
+            r.raise_for_status()
+
+    def test_run_start(self):
+        rs = dict(time=time(), scan_id=1, beamline_id='example', uid=str(uuid.uuid4()))
+        url = self.base_url + 'run_start'
+        payload = dict(data=rs, signature='insert_run_start')
+        r = requests.post(url, data = ujson.dumps(payload))
         r.raise_for_status()
 
+    def test_run_stop(self):
+        pass
 
-def teardown():
-    mds_teardown()
+    def teardown_class(self):
+        mds_teardown()
--- metadataservice/test/utils.py
+++ metadataservice/test/utils.py
@@ -4,7 +4,7 @@ import uuid
 import time as ttime
 from subprocess import Popen
 import os
-
+from pymongo import MongoClient
 
 testing_config = dict(mongohost='localhost', mongoport=27017,
                       database='mds_test'+str(uuid.uuid4()),
@@ -19,8 +19,9 @@ def mds_setup():
                   "--tzone", testing_config['tzone'], "--serviceport",
            str(testing_config['serviceport'])], cwd=f)
     ttime.sleep(1) # make sure the process is started
-    # TODO: Drop the database created using pymongo here!
 
 def mds_teardown():
     proc2 = Popen(['kill', '-9', str(proc.pid)])
     ttime.sleep(1) # make sure the process is killed
+    conn = MongoClient(host=testing_config['mongohost'], port=testing_config['mongoport'])
+    conn.drop_database(testing_config['database'])
