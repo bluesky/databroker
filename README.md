baa3f30d7118ba5a186bf9e15809e11db234bdf0
danielballan <daniel.b.allan@gmail.com>
2016-06-14 14:04:53 -0400

    MNT: Do packaging

---
+++ README.md
@@ -0,0 +1,8 @@
+# Portable Metadatastore
+
+an implementation of the metadatastore interface in front of alternative
+backends that do not require a Mongo interface
+
+* JSON headers and events
+* JSON headers and HDF5 events
+* JSON headers and sqlite events
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import versioneer
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='portable-mds',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license="BSD (3-clause)",
+    url="https://github.com/NSLS-II/portable-mds",
+    packages=find_packages(),
+    long_description=read('README.md'),
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 3',
+    ],
+)
