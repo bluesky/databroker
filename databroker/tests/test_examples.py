58918c515bbe32c9cdbfc0d8d90d69a9523c244b
Thomas A Caswell <tcaswell@bnl.gov>
2016-07-01 08:50:53 -0400

    TST: add minimal test of get_resource_uids

--- databroker/tests/test_examples.py
+++ databroker/tests/test_examples.py
@@ -1,7 +1,9 @@
 import pytest
 from filestore.test.utils import fs_setup, fs_teardown
+from filestore.fs import FileStoreMoving
+import filestore.conf
 from metadatastore.test.utils import mds_setup, mds_teardown
-
+from databroker.core import register_builtin_handlers
 from .utils import Command
 from ..examples.sample_data import (temperature_ramp, multisource_event,
                                     image_and_scalar, step_scan)
@@ -35,3 +37,23 @@ def run_example_cmdline(example):
 def test_examples_programmatically(example):
     run_example_programmatically(example)
     run_example_cmdline(example)
+
+
+def test_get_resource_uid():
+    from databroker import DataBroker as db
+    fs = FileStoreMoving(db.fs.config)
+    old_fs = db.fs
+    db.fs = fs
+    register_builtin_handlers(fs)
+    events = list(image_and_scalar.run())
+    fs_res = set()
+    run_start = events[0]['descriptor']['run_start']
+    hdr = db[run_start['uid']]
+    for ev in db.get_events(hdr, fill=False):
+        for k in ['img', 'img_sum_x', 'img_sum_y']:
+            dd = ev['data']
+            if k in dd:
+                fs_res.add(fs.resource_given_eid(dd[k])['uid'])
+
+    assert fs_res == db.get_resource_uids(hdr)
+    db.fs = old_fs
