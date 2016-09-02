53186769d1c95d1efa3c388441227a1b7381da57
danielballan <daniel.b.allan@gmail.com>
2016-09-02 15:43:45 -0400

    TST: Extend db fixture to cover sqlite and pymongo.

--- databroker/tests/conftest.py
+++ databroker/tests/conftest.py
@@ -1,15 +1,25 @@
+import uuid
 import shutil
 import os
 import tempfile
 import tzlocal
-from portable_mds.sqlite.mds import MDS
-from portable_fs.sqlite.fs import FileStore
 from databroker import Broker
 from bluesky.tests.conftest import fresh_RE as RE
 import pytest
 
-@pytest.fixture(scope='function')
+@pytest.fixture(params=['sqlite', 'mongo'], scope='function')
 def db(request):
+    param_map = {'sqlite': build_sqlite_backed_broker,
+                 'mongo': build_pymongo_backed_broker}
+
+    return param_map[request.param](request)
+
+
+def build_sqlite_backed_broker(request):
+    """Uses mongoquery + sqlite -- no pymongo or mongo server anywhere"""
+    from portable_mds.sqlite.mds import MDS
+    from portable_fs.sqlite.fs import FileStore
+
     tempdirname = tempfile.mkdtemp()
     mds = MDS({'directory': tempdirname,
                              'timezone': tzlocal.get_localzone().zone}, version=1)
@@ -32,5 +42,39 @@ def db(request):
 
     request.addfinalizer(delete_fs)
 
-    db = Broker(mds, fs)
-    return db
+    return Broker(mds, fs)
+
+
+def build_pymongo_backed_broker(request):
+    '''Provide a function level scoped MDS instance talking to
+    temporary database on localhost:27017 with v1 schema.
+
+    '''
+    from metadatastore.mds import MDS
+    from filestore.utils import create_test_database
+    from filestore.fs import FileStore
+
+    db_name = "mds_testing_disposable_{}".format(str(uuid.uuid4()))
+    test_conf = dict(database=db_name, host='localhost',
+                     port=27017, timezone='US/Eastern')
+    mds = MDS(test_conf, 1)
+
+    def delete_mds():
+        print("DROPPING DB")
+        mds._connection.drop_database(db_name)
+
+    request.addfinalizer(delete_mds)
+
+    db_name = "fs_testing_base_disposable_{uid}"
+    test_conf = create_test_database(host='localhost',
+                                     port=27017, version=1,
+                                     db_template=db_name)
+    fs = FileStore(test_conf, version=1)
+
+    def delete_fs():
+        print("DROPPING DB")
+        fs._connection.drop_database(test_conf['database'])
+
+    request.addfinalizer(delete_fs)
+
+    return Broker(mds, fs)
