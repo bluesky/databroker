8d9cc610d2d977f3d7915f01fc2dec9a019d4b59
christopher <cjwright4242@gmail.com>
2017-02-08 10:23:16 -0500

    ENH: fix filled

--- portable_mds/template/core.py
+++ portable_mds/template/core.py
@@ -372,7 +372,6 @@ def get_events_generator(descriptor, event_col, descriptor_col,
 
     data_keys = descriptor['data_keys']
     external_keys = [k for k in data_keys if 'external' in data_keys[k]]
-    filled = {k: False for k in external_keys}
     for ev in ev_cur:
         # ditch the ObjectID
         ev.pop('_id', None)
@@ -387,7 +386,7 @@ def get_events_generator(descriptor, event_col, descriptor_col,
                     ev['data'][k] = np.asarray(ev['data'][k])
 
         # note which keys refer to dereferences (external) data
-        ev['filled'] = filled
+        ev['filled'] = {k: False for k in external_keys}
 
         # wrap it in our fancy dict
         ev = doc.Document('Event', ev)
