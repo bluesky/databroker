df2f6e8a4e166d97143a5c1a251b05ada9c9e666
christopher <cjwright4242@gmail.com>
2017-02-23 10:41:09 -0500

    ENH: now portable FS commits

--- databroker/tests/test_broker.py
+++ databroker/tests/test_broker.py
@@ -608,6 +608,7 @@ def test_export(db, db2, RE):
 
     fs4 = f_s_class(db2.fs.config, version=1)
     db3 = Broker(fs=fs4, mds=db2.mds)
+    del db2
     db3.fs.register_handler('RWFS_NPY', Handler)
     assert db3[uid] == db1[uid]
     image2, = db3.get_images(db3[uid], 'image')
