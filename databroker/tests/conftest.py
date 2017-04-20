ae8a700000e969a99e6dfc6dafa3863484205d9e
christopher <cjwright4242@gmail.com>
2017-04-20 13:47:14 -0400

    TST: use mongoquery backend

--- databroker/tests/conftest.py
+++ databroker/tests/conftest.py
@@ -2,24 +2,27 @@ import sys
 import pytest
 
 from databroker.tests.utils import (build_sqlite_backed_broker,
-                                    build_pymongo_backed_broker)
+                                    build_pymongo_backed_broker,
+                                    build_json_backed_broker)
 
 if sys.version_info >= (3, 0):
     from bluesky.tests.conftest import fresh_RE as RE
 
 
-@pytest.fixture(params=['sqlite', 'mongo'], scope='function')
+@pytest.fixture(params=['sqlite', 'mongo', 'json'], scope='function')
 def db(request):
     param_map = {'sqlite': build_sqlite_backed_broker,
-                 'mongo': build_pymongo_backed_broker}
+                 'mongo': build_pymongo_backed_broker,
+                 'json': build_json_backed_broker}
 
     return param_map[request.param](request)
 
 
-@pytest.fixture(params=['sqlite', 'mongo'], scope='function')
+@pytest.fixture(params=['sqlite', 'mongo', 'json'], scope='function')
 def broker_factory(request):
     "Use this to get more than one broker in a test."
     param_map = {'sqlite': lambda: build_sqlite_backed_broker(request),
-                 'mongo': lambda: build_pymongo_backed_broker(request)}
+                 'mongo': lambda: build_pymongo_backed_broker(request),
+                 'json': lambda: build_json_backed_broker(request)}
 
     return param_map[request.param]
