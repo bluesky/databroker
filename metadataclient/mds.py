ebd3c014dd167900d20521d4380d2572e66111f2
Arman Arkilic <armanarkilic@gmail.com>
2017-05-05 14:42:36 -0400

    CI:Minor fix for travis

--- metadataclient/mds.py
+++ metadataclient/mds.py
@@ -721,6 +721,7 @@ class MDS(MDSRO):
         ret : dict
             dictionary of details about the insertion
         """
+        events = list(events) # if iterator, make json serializable
         for e in events:
             for k, v in e['data'].items():
                 e['data'][k] = _sanitize_np(v)
