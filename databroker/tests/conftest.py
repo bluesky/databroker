c4399715aed11a3e563dc45eb60765390985efb1
Thomas A Caswell <tcaswell@bnl.gov>
2017-06-15 15:17:14 -0400

    TST: use toplevel conftest for sqlite/hdf mds tests
     - remove un-used module level fixture

--- databroker/tests/conftest.py
+++ databroker/tests/conftest.py
@@ -33,46 +33,13 @@ def broker_factory(request):
 AUTH = os.environ.get('MDSTESTWITHAUTH', False)
 
 
-@pytest.fixture(params=[1], scope='function')
+@pytest.fixture(params=['sqlite', 'mongo', 'hdf5'], scope='function')
 def mds_all(request):
     '''Provide a function level scoped FileStore instance talking to
     temporary database on localhost:27017 with both v0 and v1.
     '''
-    db_name = "mds_testing_disposable_{}".format(str(uuid.uuid4()))
-    test_conf = dict(database=db_name, host='localhost',
-                     port=27017, timezone='US/Eastern',
-                     mongo_user='tom',
-                     mongo_pwd='jerry')
-    ver = request.param
-    mds = MDS(test_conf, ver, auth=AUTH)
+    param_map = {'sqlite': build_sqlite_backed_broker,
+                 'mongo': build_pymongo_backed_broker,
+                 'hdf5': build_hdf5_backed_broker}
 
-    def delete_dm():
-        print("DROPPING DB")
-        mds._connection.drop_database(db_name)
-
-    request.addfinalizer(delete_dm)
-
-    return mds
-
-
-@pytest.fixture(params=[1], scope='module')
-def mds_all_mod(request):
-    '''Provide a function level scoped FileStore instance talking to
-    temporary database on localhost:27017 with both v0 and v1.
-    '''
-    db_name = "mds_testing_disposable_{}".format(str(uuid.uuid4()))
-    test_conf = dict(database=db_name, host='localhost',
-                     port=27017, timezone='US/Eastern',
-                     mongo_user='tom',
-                     mongo_pwd='jerry')
-    ver = request.param
-
-    mds = MDS(test_conf, ver, auth=AUTH)
-
-    def delete_dm():
-        print("DROPPING DB")
-        mds._connection.drop_database(db_name)
-
-    request.addfinalizer(delete_dm)
-
-    return mds
+    return param_map[request.param](request).mds
