92d760b29a914711446bdd9424c81ab673c84298
Thomas A Caswell <tcaswell@bnl.gov>
2017-08-07 14:55:10 -0400

    TST: smoke test mongo reconfigure / disconnect

--- databroker/resource_registry/tests/test_mongo_details.py
+++ databroker/resource_registry/tests/test_mongo_details.py
@@ -19,3 +19,8 @@ def test_index(fs_mongo):
     assert len(indx) == 3
     index_fields = set(v['key'][0][0] for v in indx.values())
     assert index_fields == {'_id', 'datum_id', 'resource'}
+
+
+def test_reconfigure(fs_mongo):
+    fs = fs_mongo
+    fs.reconfigure(fs.config)
