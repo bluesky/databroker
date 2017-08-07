892ea917793ceaf04291de5896ced973eb358e5f
Thomas A Caswell <tcaswell@bnl.gov>
2017-08-07 19:15:04 -0400

    MNT: use the FileStoreTemplate in mongo classes

--- databroker/resource_registry/mongo.py
+++ databroker/resource_registry/mongo.py
@@ -1,7 +1,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-import json
 import logging
 import os
 import os.path
@@ -12,20 +11,16 @@ import warnings
 import boltons.cacheutils
 import pymongo
 import six
-from pkg_resources import resource_filename
 from pymongo import MongoClient
 
 from . import mongo_core
-from .handlers_base import DuplicateHandler
 from ..utils import _make_sure_path_exists
-from .base import _ChainMap
-_API_MAP = {1: mongo_core}
-
+from .base import _ChainMap, FileStoreTemplateRO
 
 logger = logging.getLogger(__name__)
 
 
-class FileStoreRO(object):
+class FileStoreRO(FileStoreTemplateRO):
     '''Base FileStore object that knows how to read the database.
 
     Parameters
@@ -47,53 +42,7 @@ class FileStoreRO(object):
         Handler in ``get_spec_handler``
 
     '''
-    KNOWN_SPEC = dict()
-    # load the built-in schema
-    for spec_name in ['AD_HDF5', 'AD_SPE']:
-        tmp_dict = {}
-        base_name = 'resource_registry/json/'
-        resource_name = '{}{}_resource.json'.format(base_name, spec_name)
-        datum_name = '{}{}_datum.json'.format(base_name, spec_name)
-        with open(resource_filename('databroker',
-                                    resource_name), 'r') as fin:
-            tmp_dict['resource'] = json.load(fin)
-        with open(resource_filename('databroker',
-                                    datum_name), 'r') as fin:
-            tmp_dict['datum'] = json.load(fin)
-        KNOWN_SPEC[spec_name] = tmp_dict
-
-    def set_root_map(self, root_map):
-        '''Set the root map
-
-        Parameters
-        ----------
-        root_map : dict
-            str -> str mapping to account for temporarily
-            moved/copied/remounted files.  Any resources which have a
-            ``root`` in ``root_map`` will have the resource path
-            updated before being handed to the Handler in
-            ``get_spec_handler``
-        '''
-        self.root_map = root_map
-
-    @property
-    def version(self):
-        return self._version
-
-    @version.setter
-    def version(self, val):
-        if self._api is not None:
-            raise RuntimeError("Can not change api version at runtime")
-        self._api = _API_MAP[val]
-        self._version = val
-
-    @property
-    def DatumNotFound(self):
-        return self._api.DatumNotFound
-
-    @property
-    def DuplicateKeyError(self):
-        return self._api.DuplicateKeyError
+    _API_MAP = {1: mongo_core}
 
     def __init__(self, config, handler_reg=None, version=1, root_map=None):
         self.config = config
@@ -127,56 +76,6 @@ class FileStoreRO(object):
     def reconfigure(self, config):
         self.disconnect()
         self.config = config
-
-    def _r_on_miss(self, k):
-        col = self._resource_col
-        return self._api.resource_given_uid(col, k)
-
-    def resource_given_uid(self, uid):
-        col = self._resource_col
-        return self._api.resource_given_uid(col, uid)
-
-    def retrieve(self, eid):
-        return self._api.retrieve(self._datum_col, eid,
-                                  self._datum_cache, self.get_spec_handler,
-                                  logger)
-
-    # back compat
-    get_datum = retrieve
-
-    def register_handler(self, key, handler, overwrite=False):
-        if (not overwrite) and (key in self.handler_reg):
-            if self.handler_reg[key] is handler:
-                return
-            raise DuplicateHandler(
-                "You are trying to register a second handler "
-                "for spec {}, {}".format(key, self))
-
-        self.deregister_handler(key)
-        self.handler_reg[key] = handler
-
-    def deregister_handler(self, key):
-        handler = self.handler_reg.pop(key, None)
-        if handler is not None:
-            name = handler.__name__
-            for k in list(self._handler_cache):
-                if k[1] == name:
-                    del self._handler_cache[k]
-
-    @contextmanager
-    def handler_context(self, temp_handlers):
-        stash = self.handler_reg
-        self.handler_reg = self.handler_reg.new_child(temp_handlers)
-        try:
-            yield self
-        finally:
-            popped_reg = self.handler_reg.maps[0]
-            self.handler_reg = stash
-            for handler in popped_reg.values():
-                name = handler.__name__
-                for k in list(self._handler_cache):
-                    if k[1] == name:
-                        del self._handler_cache[k]
 
     @property
     def _db(self):
@@ -234,204 +133,9 @@ class FileStoreRO(object):
                                       self.config.get('port', None))
         return self.__conn
 
-    def get_spec_handler(self, resource):
-        """
-        Given a document from the base FS collection return
-        the proper Handler
-
-        This should get memozied or shoved into a class eventually
-        to minimize open/close thrashing.
-
-        Parameters
-        ----------
-        resource : ObjectId
-            ObjectId of a resource document
-
-        Returns
-        -------
-
-        handler : callable
-            An object that when called with the values in the event
-            document returns the externally stored data
-
-        """
-        resource = self._resource_cache[resource]
-
-        h_cache = self._handler_cache
-
-        spec = resource['spec']
-        handler = self.handler_reg[spec]
-
-        key = (str(resource['uid']), handler.__name__)
-
-        try:
-            return h_cache[key]
-        except KeyError:
-            pass
-
-        kwargs = resource['resource_kwargs']
-        rpath = resource['resource_path']
-        root = resource.get('root', '')
-        root = self.root_map.get(root, root)
-        if root:
-            rpath = os.path.join(root, rpath)
-        ret = handler(rpath, **kwargs)
-        h_cache[key] = ret
-        return ret
-
-    def get_history(self, resource_uid):
-        '''Generator of all updates to the given resource
-
-        Parameters
-        ----------
-        resource_uid : Document or str
-            The resource to get the history of
-
-        Yields
-        ------
-        update : Document
-        '''
-        if self.version == 0:
-            raise NotImplementedError('No history in v0 schema')
-
-        for doc in self._api.get_resource_history(
-                self._resource_update_col, resource_uid):
-            yield doc
-
-    def copy_files(self, resource_or_uid, new_root,
-                   verify=False, file_rename_hook=None):
-        """
-        Copy files associated with a resource to a new directory.
-
-        The registered handler must have a `get_file_list` method and the
-        process running this method must have read/write access to both the
-        source and destination file systems.
-
-        This method does *not* update the filestore database.
-
-        Internally the resource level directory information is stored
-        as two parts: the root and the resource_path.  The 'root' is
-        the non-semantic component (typically a mount point) and the
-        'resource_path' is the 'semantic' part of the file path.  For
-        example, it is common to collect data into paths that look like
-        ``/mnt/DATA/2016/04/28``.  In this case we could split this as
-        ``/mnt/DATA`` as the 'root' and ``2016/04/28`` as the resource_path.
-
-
-
-        Parameters
-        ----------
-        resource_or_uid : Document or str
-            The resource to move the files of
-
-        new_root : str
-            The new 'root' to copy the files into
-
-        verify : bool, optional (False)
-            Verify that the move happened correctly.  This currently
-            is not implemented and will raise if ``verify == True``.
-
-        file_rename_hook : callable, optional
-            If provided, must be a callable with signature ::
-
-               def hook(file_counter, total_number, old_name, new_name):
-                   pass
-
-            This will be run in the inner loop of the file copy step and is
-            run inside of an unconditional try/except block.
-
-        See Also
-        --------
-        `FileStoreMoving.shift_root`
-        `FileStoreMoving.change_root`
-        """
-        if self.version == 0:
-            raise NotImplementedError('V0 has no notion of root so can not '
-                                      'change it')
-        if verify:
-            raise NotImplementedError('Verification is not implemented yet')
-
-        def rename_hook_wrapper(hook):
-            if hook is None:
-                def noop(n, total, old_name, new_name):
-                    return
-                return noop
-
-            def safe_hook(n, total, old_name, new_name):
-                try:
-                    hook(n, total, old_name, new_name)
-                except:
-                    pass
-            return safe_hook
-
-        file_rename_hook = rename_hook_wrapper(file_rename_hook)
-
-        # get list of files
-        resource = dict(self.resource_given_uid(resource_or_uid))
-
-        datum_gen = self.datum_gen_given_resource(resource)
-        datum_kwarg_gen = (datum['datum_kwargs'] for datum in datum_gen)
-        file_list = self.get_file_list(resource, datum_kwarg_gen)
-
-        # check that all files share the same root
-        old_root = resource.get('root')
-        if not old_root:
-            warnings.warn("There is no 'root' in this resource which "
-                          "is required to be able to change the root. "
-                          "Please use `fs.shift_root` to move some of "
-                          "the path from the 'resource_path' to the "
-                          "'root'.  For now assuming '/' as root")
-            old_root = os.path.sep
-
-        for f in file_list:
-            if not f.startswith(old_root):
-                raise RuntimeError('something is very wrong, the files '
-                                   'do not all share the same root, ABORT')
-
-        # sort out where new files should go
-        new_file_list = [os.path.join(new_root,
-                                      os.path.relpath(f, old_root))
-                         for f in file_list]
-        N = len(new_file_list)
-        # copy the files to the new location
-        for n, (fin, fout) in enumerate(zip(file_list, new_file_list)):
-            # copy files
-            file_rename_hook(n, N, fin, fout)
-            _make_sure_path_exists(os.path.dirname(fout))
-            shutil.copy2(fin, fout)
-
-        return zip(file_list, new_file_list)
-
-    def datum_gen_given_resource(self, resource_or_uid):
-        """Given resource or resource uid return associated datum documents.
-        """
-        actual_resource = self.resource_given_uid(resource_or_uid)
-        datum_gen = self._api.get_datum_by_res_gen(self._datum_col,
-                                                   actual_resource['uid'])
-        return datum_gen
-
-    def get_file_list(self, resource_or_uid, datum_kwarg_gen):
-        """Given a resource or resource uid and an iterable of datum kwargs,
-        return filepaths.
-
-
-        DO NOT USE FOR COPYING OR MOVING. This is for debugging only.
-        See the methods for moving and copying on the FileStore object.
-        """
-        actual_resource = self.resource_given_uid(resource_or_uid)
-        return self._api.get_file_list(actual_resource, datum_kwarg_gen,
-                                       self.get_spec_handler)
-
-    def resource_given_eid(self, eid):
-        '''Given a datum eid return its Resource document
-        '''
-        if self.version == 0:
-            raise NotImplementedError('V0 has no notion of root so can not '
-                                      'change it so no need for this method')
-
-        res = self._api.resource_given_eid(self._datum_col, eid,
-                                           self._datum_cache, logger)
-        return self._resource_cache[res]
+    @property
+    def DuplicateKeyError(self):
+        return self._api.DuplicateKeyError
 
 
 class FileStore(FileStoreRO):
