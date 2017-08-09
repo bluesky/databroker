f4500a1701be181c6bad178e180c452db5ff99e5
Thomas A Caswell <tcaswell@bnl.gov>
2017-08-09 16:08:15 -0400

    ENH: let our makedirs shim take exists_ok as an input

--- databroker/utils.py
+++ databroker/utils.py
@@ -4,14 +4,14 @@ import os
 
 if six.PY2:
     # http://stackoverflow.com/a/5032238/380231
-    def ensure_path_exists(path):
+    def ensure_path_exists(path, exist_ok=True):
         import errno
         try:
             os.makedirs(path)
         except OSError as exception:
-            if exception.errno != errno.EEXIST:
+            if exception.errno != errno.EEXIST or not exist_ok:
                 raise
 else:
     # technically, this won't work with py3.1, but no one uses that
-    def ensure_path_exists(path):
+    def ensure_path_exists(path, exist_ok=True):
         return os.makedirs(path, exist_ok=True)
