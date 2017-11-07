e949a2e0794405c05b811ee82d7902e4779ef046
Thomas A Caswell <tcaswell@bnl.gov>
2017-11-07 14:00:53 -0500

    ENH: first pass at creating glue Data objects from Headers

---
+++ databroker/glue.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, division, print_function
+
+from glue.core.data import Data, Component
+
+
+def read_header(header):
+    out = []
+    for stream in header.stream_names:
+        result = Data(label=f"{stream}_{header.start['uid']}")
+        tbl = header.table(stream)
+        for col in tbl:
+            result.add_component(Component(tbl[col].values), col)
+        out.append(result)
+
+    return out
