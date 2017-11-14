9b15bb8d160820e9e594fcdf7f4d1dddd9b8de56
Maksim Rakitin <maxim.rakitin@gmail.com>
2017-11-14 18:03:00 -0500

    TST: Added test for glue.

---
+++ databroker/tests/test_glue.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+from ophyd.sim import SynAxis, SynGauss
+from bluesky import RunEngine
+import bluesky.plans as bp
+from databroker import temp_config, Broker
+from databroker.glue import read_header
+from glue.qglue import parse_data
+
+def test_glue(db, RE):
+    motor = SynAxis(name='motor')
+    det = SynGauss('det', motor, 'motor', center=0, Imax=1,
+                   noise='uniform', sigma=1, noise_multiplier=0.1)
+    RE.subscribe(db.insert)
+    RE(bp.scan([det], motor, -5, 5, 10))
+
+    d = read_header(db[-1])
+    g = parse_data(d[0], 'test')[0].to_dataframe()
+    
