5570abb035397ddcd7c3d5cd4e07b579145ccd25
nmalitsky <nmalitsky@gmail.com>
2018-08-22 19:19:37 -0400

    updated archiver.py with recent comments

--- databroker/eventsource/archiver.py
+++ databroker/eventsource/archiver.py
@@ -5,7 +5,6 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import six
 
-from functools import partial
 from datetime import datetime
 import pytz
 import uuid
@@ -17,8 +16,9 @@ import pandas as pd
 
 from ..utils import ALL
 
+
 class ArchiverEventSource(object):
-    
+
     def __init__(self, config):
         """
         Shim class to turn the EPICS Archiver Appliance into EventSource
@@ -29,10 +29,10 @@ class ArchiverEventSource(object):
         else:
             raise TypeError("config {} does not include one of required"
                             " keys (name, url, timezone).".format(config))
-        
+
         self._name = config['name']
-        
-        url =  config['url']
+
+        url = config['url']
         if not url.endswith('/'):
             url += '/'
         self.url = url
@@ -46,9 +46,8 @@ class ArchiverEventSource(object):
         else:
             pvs = {}
         self.pvs = pvs
-        
-        self._descriptors = {}
 
+        self._descriptors = {}
 
     @property
     def name(self):
@@ -58,7 +57,7 @@ class ArchiverEventSource(object):
         """
         Extend a dictionary mapping user-defined names to EPICS PV's
         """
-        
+
         self.pvs.update(dict(new_pvs))
         return dict(self.pvs)
 
@@ -66,14 +65,18 @@ class ArchiverEventSource(object):
         """
         Return a dictionary mapping user-defined names to EPICS PV's
         """
-        
+
         return dict(self.pvs)
 
     def insert(self, name, doc):
-         """
-         Not supported, data archiving is managed via the EPICS Archiver
-         """
-         raise TypeError("Not supported, data archiving is managed via the EPICS Archiver Appliance Toolkit")
+        """
+        Not supported, data archiving is managed via the EPICS Archiver
+        """
+        msg = "Not supported, data archiving is managed via"
+        msg += " the EPICS Archiver Appliance Toolkit"
+        raise NotImplementedError("Not supported, data archiving is "
+                                  "managed via the EPICS Archiver "
+                                  "Appliance Toolkit")
 
     def stream_names_given_header(self, header):
         # We actually don't use the header in this case.
@@ -89,25 +92,25 @@ class ArchiverEventSource(object):
         -------
         list : list of user-defined PV names
         """
-            
+
         return list(self.pvs)
 
     def fields_given_header(self, header):
         # We actually don't use the header in this case.
         """
-        Return a set of user-defined PV names
+        Return an empty set since fields are not used
 
         Parameters
         ----------
         header : Header
-            not used. fields are generated from pvs
+            not used. 
 
         Returns
         -------
-        set : set of user-defined PV names
+        set : empty set
         """
-               
-        return set(self.pvs)
+
+        return set()
 
     def descriptors_given_header(self, header):
         """
@@ -122,9 +125,9 @@ class ArchiverEventSource(object):
         list : list of PV descriptors
         """
         run_start_uid = header['start']['uid']
-        
+
         try:
-            return self._descriptors[run_start_uid]
+            return list(self._descriptors[run_start_uid])
         except KeyError:
             # Mock up descriptors and cache them so that the ephemeral uid is
             # stable for the duration of this process.
@@ -139,7 +142,7 @@ class ArchiverEventSource(object):
                 # because since is a tuple^
                 _to = _munge_time(until, self.tz)
                 params = {'pv': pv, 'from': _from, 'to': _to}
-                desc = {'name' : name,
+                desc = {'name': name,
                         'time': header['start']['time'],
                         'uid': 'empheral-' + str(uuid.uuid4()),
                         'data_keys': data_keys,
@@ -148,7 +151,7 @@ class ArchiverEventSource(object):
                         'external_url': self.url}
                 descs.append(desc)
             self._descriptors[run_start_uid] = descs
-            return list(self._descriptors[run_start_uid])     
+            return list(self._descriptors[run_start_uid])
 
     def docs_given_header(self, header, stream_name=ALL, fields=None):
         """
@@ -178,28 +181,30 @@ class ArchiverEventSource(object):
         _to = _munge_time(until, self.tz)
 
         yield 'start', header['start']
-        
+
         for d in self.descriptors_given_header(header):
 
             if d['name'] == stream_name:
 
                 yield 'descriptor', d
-            
-                # Stash the desc uids in a local var so we can use them in events.
+
+                # Stash the desc uids in a local var
+                # so we can use them in events.
                 name = list(d['data_keys'].keys())[0]
                 pv = list(d['data_keys'].values())[0]['source']
                 desc_uids[pv] = d['uid']
-            
+
                 params = {'pv': pv, 'from': _from, 'to': _to}
-            
-                req = requests.get(self.archiver_addr, params=params, stream=True)
+
+                req = requests.get(self.archiver_addr, params=params,
+                                   stream=True)
                 req.raise_for_status()
                 raw, = req.json()
-            
-                timestamps = [x['secs'] for x in raw['data']]
+
+                ts = [x['secs'] for x in raw['data']]
                 data = [x['val'] for x in raw['data']]
-            
-                for seq_num, (v, t) in enumerate(zip(data, timestamps), start=1):
+
+                for seq_num, (v, t) in enumerate(zip(data, ts), start=1):
                     doc = {'data': {name: v},
                            'timestamps': {name: t},
                            'time': t,
@@ -207,22 +212,23 @@ class ArchiverEventSource(object):
                            'seq_num': seq_num,
                            'descriptor': desc_uids[pv]}
                     yield 'event', doc
+
             else:
                 continue
 
         yield 'stop', header['stop']
 
     def _table_given_times(self, pv, since, until):
-        
+
         _from = _munge_time(since, self.tz)
         _to = _munge_time(until, self.tz)
 
         params = {'pv': pv, 'from': _from, 'to': _to}
-            
+
         req = requests.get(self.archiver_addr, params=params, stream=True)
         req.raise_for_status()
         raw, = req.json()
-            
+
         secs = [x['secs'] for x in raw['data']]
         nanos = [x['nanos'] for x in raw['data']]
         data = [x['val'] for x in raw['data']]
@@ -259,12 +265,12 @@ class ArchiverEventSource(object):
         dfs = {}
         for key in self.pvs.keys():
             dfs[key] = self._table_given_times(self.pvs[key], since, until)
-            
+
         return dfs
- 
-    def table_given_header(self, header, stream_name = ALL,
-                            fields=None, convert_times=True,
-                            timezone=None, localize_times=True):
+
+    def table_given_header(self, header, stream_name=ALL,
+                           fields=None, convert_times=True,
+                           timezone=None, localize_times=True):
 
         """
         Make PV table (pandas.DataFrame) from given Header
@@ -273,7 +279,7 @@ class ArchiverEventSource(object):
         ----------
         header : Header
             The header to fetch the table for
-        stream_name : string, 
+        stream_name : string,
             PV stream_name (e.g., archiver_PV1)
         fields : list, not used
             names of interest are defined via user-defined PVs
@@ -285,32 +291,33 @@ class ArchiverEventSource(object):
             e.g., 'US/Eastern'
         localize_times : bool, optional
             If the times should be localized to the 'local' time zone.  If
-            True (the default) the time stamps are converted to the localtime zone.
+            True (the default) the time stamps are converted to
+            the localtime zone.
 
         Returns
         -------
         tables : dictionary of the pv pandas.DataFrames
         """
-        
+
         if timezone is None:
             timezone = self.tz.zone
 
-        desc_uids = {}
-
         since, until = header['start']['time'], header['stop']['time']
-  
+
         df = pd.DataFrame()
 
         if stream_name in self.stream_names_given_header(header):
             name = stream_name
             pv = self.pvs[name]
             df = self._table_given_times(pv, since, until)
-            df.rename(columns = {"time": "time", "data" : stream_name}, inplace=True)
-            new_index = np.arange(1, df.index.size +1)
+            df.rename(columns={"time": "time", "data": stream_name},
+                      inplace=True)
+            new_index = np.arange(1, df.index.size + 1)
             df.index = new_index
 
         return df
- 
+
+
 def _munge_time(t, timezone):
     """Close your eyes and trust @arkilic
 
@@ -328,4 +335,3 @@ def _munge_time(t, timezone):
     """
     t = datetime.fromtimestamp(t)
     return timezone.localize(t).replace(microsecond=0).isoformat()
-
