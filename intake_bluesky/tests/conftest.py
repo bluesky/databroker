5baf965f7d7ba94d74b999af8e24b1284ca10624
Dan Allan <dallan@bnl.gov>
2019-02-14 11:25:50 -0500

    MNT: Avoid picking up extraneous dependencies.

--- intake_bluesky/tests/conftest.py
+++ intake_bluesky/tests/conftest.py
@@ -1,3 +1,3 @@
 from intake.conftest import intake_server  # noqa
-from suitcase.mongo_layout1.tests.conftest import db_factory  # noqa
+from suitcase.mongo_layout1.tests.fixtures import db_factory  # noqa
 
