80b8a7d2c72081824ef09664842cb05e741758ee
Dan Allan <dallan@bnl.gov>
2019-05-02 16:31:00 -0400

    STY: whitespace

--- intake_bluesky/tests/test_core.py
+++ intake_bluesky/tests/test_core.py
@@ -6,7 +6,9 @@ def test_no_descriptors():
     run_bundle = event_model.compose_run()
     start_doc = run_bundle.start_doc
     stop_doc = run_bundle.compose_stop()
-    documents_to_xarray(start_doc=start_doc, stop_doc=stop_doc,
+    documents_to_xarray(
+        start_doc=start_doc,
+        stop_doc=stop_doc,
         descriptor_docs=[],
         event_docs=[],
         filler=event_model.Filler({}),
@@ -23,7 +25,9 @@ def test_no_events():
         name='primary')
     descriptor_doc = desc_bundle.descriptor_doc
     stop_doc = run_bundle.compose_stop()
-    documents_to_xarray(start_doc=start_doc, stop_doc=stop_doc,
+    documents_to_xarray(
+        start_doc=start_doc,
+        stop_doc=stop_doc,
         descriptor_docs=[descriptor_doc],
         event_docs=[],
         filler=event_model.Filler({}),
