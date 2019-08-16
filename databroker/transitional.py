bad39202e383c4ffd3285e74ae9841655adf107b
Dan Allan <dallan@bnl.gov>
2019-08-16 11:35:42 -0400

    Use describe() to access metadata.

--- databroker/transitional.py
+++ databroker/transitional.py
@@ -582,7 +582,7 @@ class Header:
     def descriptors(self):
         descriptors = []
         for name, entry in self._data_source._entries.items():
-            descriptors.extend(entry.metadata['descriptors'])
+            descriptors.extend(entry.describe()['metadata']['descriptors'])
         return [self.db.prepare_hook('descriptor', doc)
                 for doc in descriptors]
 
