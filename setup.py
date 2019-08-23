6f5b0c215dff28139f8fd06a0efc6e2202701634
Dan Allan <dallan@bnl.gov>
2019-08-23 16:09:35 -0400

    Merge intake-bluesky's packaging-related files into ours.

--- setup.py
+++ setup.py
@@ -5,6 +5,24 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
+# NOTE: This file must remain Python 2 compatible for the foreseeable future,
+# to ensure that we error out properly for people with outdated setuptools
+# and/or pip.
+min_version = (3, 6)
+if sys.version_info < min_version:
+    error = """
+intake-bluesky does not support Python {0}.{1}.
+Python {2}.{3} and above is required. Check your Python version like so:
+
+python3 --version
+
+This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
+Upgrade pip like so:
+
+pip install --upgrade pip
+""".format(*sys.version_info[:2], *min_version)
+    sys.exit(error)
+
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
@@ -42,6 +60,23 @@ setup(
 
     install_requires=requirements,
     extras_require=extras_require,
+    python_requires='>={}'.format('.'.join(str(n) for n in min_version)),
+
+    entry_points={
+        'console_scripts': [
+            # 'command = some.module:some_function',
+        ],
+        'intake.drivers': [
+            'bluesky-event-stream = intake_bluesky.core:BlueskyEventStream',
+            'bluesky-jsonl-catalog = intake_bluesky.jsonl:BlueskyJSONLCatalog',
+            ('bluesky-mongo-embedded-catalog = '
+             'intake_bluesky.mongo_embedded:BlueskyMongoCatalog'),
+            ('bluesky-mongo-normalized-catalog = '
+             'intake_bluesky.mongo_normalized:BlueskyMongoCatalog'),
+            'bluesky-msgpack-catalog = intake_bluesky.msgpack:BlueskyMsgpackCatalog',
+            'bluesky-run = intake_bluesky.core:BlueskyRun',
+        ]
+    },
 
     classifiers=[
         'License :: OSI Approved :: BSD License',
