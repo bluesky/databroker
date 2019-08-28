7c63630cd22ca6d9ef3490f7f626842575d80125
Garrett Bischof <gbischof@bnl.gov>
2019-08-28 13:36:09 -0400

    minor fix to get_documents

--- databroker/v1.py
+++ databroker/v1.py
@@ -449,7 +449,7 @@ class Broker:
                     elif name == 'datum_page':
                         for datum in event_model.unpack_datum_page(doc):
                             yield 'datum', self.prepare_hook('datum', datum)
-                    else:
+                    elif name == 'datum':
                         yield 'datum', self.prepare_hook('datum', doc)
             else:
                 for name, doc in self._catalog[uid].canonical(fill=_FILL[fill]):
