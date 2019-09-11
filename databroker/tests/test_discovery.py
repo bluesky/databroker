82491516233c522e485d4ceb43d7be263777c274
Garrett Bischof <gbischof@bnl.gov>
2019-09-11 16:56:06 -0400

    collision is not giving a warning

--- databroker/tests/test_discovery.py
+++ databroker/tests/test_discovery.py
@@ -1,14 +1,15 @@
 from ..discovery import MergedCatalog, EntrypointsCatalog, V0Catalog
 import copy
 import os
-
+import glob
 
 def test_catalog_discovery():
     basedir = os.path.dirname(__file__)
     path = os.path.join(basedir, 'catalog_search')
-    print(path)
+    collision_path = os.path.join(path, 'v0')
+
     test_catalog = MergedCatalog([EntrypointsCatalog(paths=[path]),
-                             V0Catalog(paths=[path])])
+                             V0Catalog(paths=[path, collision_path])])
 
     assert 'v0' in test_catalog
     assert 'ep1' in test_catalog
