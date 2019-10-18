52492b6ec70636c3b4bc58824f5b714a23d8fd4d
Garrett Bischof <gbischof@bnl.gov>
2019-10-18 15:36:32 -0400

    updated msgpack driver

--- databroker/_drivers/msgpack.py
+++ databroker/_drivers/msgpack.py
@@ -68,11 +68,39 @@ class BlueskyMsgpackCatalog(BlueskyInMemoryCatalog):
         paths : list
             list of filepaths
         handler_registry : dict, optional
-            Maps each asset spec to a handler class or a string specifying the
-            module name and class name, as in (for example)
-            ``{'SOME_SPEC': 'module.submodule.class_name'}``.
-        root_map : dict, optional
-            Maps resource root paths to different paths.
+            This is passed to the Filler or whatever class is given in the
+            filler_class parametr below.
+
+            Maps each 'spec' (a string identifying a given type or external
+            resource) to a handler class.
+
+            A 'handler class' may be any callable with the signature::
+
+                handler_class(resource_path, root, **resource_kwargs)
+
+            It is expected to return an object, a 'handler instance', which is also
+            callable and has the following signature::
+
+            handler_instance(**datum_kwargs)
+
+            As the names 'handler class' and 'handler instance' suggest, this is
+            typically implemented using a class that implements ``__init__`` and
+            ``__call__``, with the respective signatures. But in general it may be
+            any callable-that-returns-a-callable.
+        root_map: dict, optional
+            This is passed to Filler or whatever class is given in the filler_class
+            parameter below.
+            str -> str mapping to account for temporarily moved/copied/remounted
+            files.  Any resources which have a ``root`` in ``root_map`` will be
+            loaded using the mapped ``root``.
+        filler_class: type, optional
+            This is Filler by default. It can be a Filler subclass,
+            ``functools.partial(Filler, ...)``, or any class that provides the
+            same methods as ``DocumentRouter``.
+        delayed_filler_class: type, optional
+            This is DaskFiller by default. It can be a Filler subclass,
+            ``functools.partial(DaskFiller, ...)``, or any class that provides the
+            same methods as ``DocumentRouter``.
         query : dict, optional
             Mongo query that filters entries' RunStart documents
         **kwargs :
