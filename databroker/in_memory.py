1d23e159f0df988d7acc31d320e5078ace41f583
Garrett <bisc8233@gmail.com>
2019-11-14 11:03:43 -0500

    fix to InMemoryCatalog kwargs

--- databroker/in_memory.py
+++ databroker/in_memory.py
@@ -13,7 +13,8 @@ from mongoquery import Query
 class BlueskyInMemoryCatalog(Broker):
     name = 'bluesky-run-catalog'  # noqa
 
-    def __init__(self, *, query=None, **kwargs):
+    def __init__(self, *, handler_registry=None, root_map=None,
+                 filler_class=event_model.Filler, query=None, **kwargs):
         """
         This Catalog is backed by Python collections in memory.
 
@@ -23,6 +24,37 @@ class BlueskyInMemoryCatalog(Broker):
 
         Parameters
         ----------
+        handler_registry : dict, optional
+            This is passed to the Filler or whatever class is given in the
+            ``filler_class`` parameter below.
+
+            Maps each 'spec' (a string identifying a given type or external
+            resource) to a handler class.
+
+            A 'handler class' may be any callable with the signature::
+
+                handler_class(resource_path, root, **resource_kwargs)
+
+            It is expected to return an object, a 'handler instance', which is also
+            callable and has the following signature::
+
+            handler_instance(**datum_kwargs)
+
+            As the names 'handler class' and 'handler instance' suggest, this is
+            typically implemented using a class that implements ``__init__`` and
+            ``__call__``, with the respective signatures. But in general it may be
+            any callable-that-returns-a-callable.
+        root_map: dict, optional
+            This is passed to Filler or whatever class is given in the
+            ``filler_class`` parameter below.
+
+            str -> str mapping to account for temporarily moved/copied/remounted
+            files.  Any resources which have a ``root`` in ``root_map`` will be
+            loaded using the mapped ``root``.
+        filler_class: type, optional
+            This is Filler by default. It can be a Filler subclass,
+            ``functools.partial(Filler, ...)``, or any class that provides the
+            same methods as ``DocumentRouter``.
         query : dict, optional
             Mongo query that filters entries' RunStart documents
         **kwargs :
@@ -32,7 +64,10 @@ class BlueskyInMemoryCatalog(Broker):
         self._query = query or {}
         self._uid_to_run_start_doc = {}
 
-        super().__init__(**kwargs)
+        super().__init__(handler_registry=handler_registry,
+                         root_map=root_map,
+                         filler_class=filler_class,
+                         **kwargs)
 
     def upsert(self, start_doc, stop_doc, gen_func, gen_args, gen_kwargs):
         if not Query(self._query).match(start_doc):
