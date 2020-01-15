c630a7500ff5c053a2f7ff40c85724f662624af4
Garrett Bischof <gbischof@bnl.gov>
2020-01-15 12:14:31 -0500

    added another lazy map test

--- databroker/tests/test_lazymap.py
+++ databroker/tests/test_lazymap.py
@@ -30,3 +30,25 @@ def test_lazymap():
 
     assert loaded['A'] is 1
     assert loaded['B'] is 1
+
+
+def test_lazymap_contains():
+    loaded = {'A': 0, 'B': 0}
+
+    class TestClass():
+        def __init__(self, value):
+            self.value = value
+            loaded[value] += 1
+
+    lazy_map = LazyMap({'A': lambda: TestClass('A'),
+                        'B': lambda: TestClass('B')})
+
+    assert loaded['A'] is 0
+    assert loaded['B'] is 0
+
+    'A' in lazy_map
+    'B' in lazy_map
+
+    assert loaded['A'] is 0
+    assert loaded['B'] is 0
+
