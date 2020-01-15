1fb815e21d4dad728fc7c0f6f0a75bf0c0588b10
Garrett Bischof <gbischof@bnl.gov>
2020-01-15 12:33:33 -0500

    changed is to == in lazymap tests

--- databroker/tests/test_lazymap.py
+++ databroker/tests/test_lazymap.py
@@ -12,24 +12,24 @@ def test_lazymap():
     lazy_map = LazyMap({'A': lambda: TestClass('A'),
                         'B': lambda: TestClass('B')})
 
-    assert loaded['A'] is 0
-    assert loaded['B'] is 0
+    assert loaded['A'] == 0
+    assert loaded['B'] == 0
 
     assert isinstance(lazy_map['A'], TestClass)
 
-    assert loaded['A'] is 1
-    assert loaded['B'] is 0
+    assert loaded['A'] == 1
+    assert loaded['B'] == 0
 
     assert isinstance(lazy_map['B'], TestClass)
 
-    assert loaded['A'] is 1
-    assert loaded['B'] is 1
+    assert loaded['A'] == 1
+    assert loaded['B'] == 1
 
     lazy_map['A']
     lazy_map['B']
 
-    assert loaded['A'] is 1
-    assert loaded['B'] is 1
+    assert loaded['A'] == 1
+    assert loaded['B'] == 1
 
 
 def test_lazymap_contains():
@@ -43,12 +43,12 @@ def test_lazymap_contains():
     lazy_map = LazyMap({'A': lambda: TestClass('A'),
                         'B': lambda: TestClass('B')})
 
-    assert loaded['A'] is 0
-    assert loaded['B'] is 0
+    assert loaded['A'] == 0
+    assert loaded['B'] == 0
 
     'A' in lazy_map
     'B' in lazy_map
 
-    assert loaded['A'] is 0
-    assert loaded['B'] is 0
+    assert loaded['A'] == 0
+    assert loaded['B'] == 0
 
