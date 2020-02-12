6a04e03fa5fa612bd95fa786f7d51b5d822558b4
Dan Allan <dallan@bnl.gov>
2020-02-12 12:31:17 -0500

    Add emergency fallback path.

--- databroker/core.py
+++ databroker/core.py
@@ -1590,6 +1590,23 @@ def _transpose(in_data, keys, field):
             # out as plain numpy arrays. In the future we might make them dask
             # arrays with manual chunks.
             out[k] = numpy.asarray(out[k])
+        except ValueError as err:
+            # TEMPORARY EMERGENCY FALLBACK
+            # If environment variable is set to anything but 0, work around
+            # dask and return a numpy array.
+            switch = int(os.environ.get('DATABROKER_ARRAY_FALLBACK'))
+            if switch:
+                out[k] = numpy.asarray(out[k])
+                warnings.warn(
+                    f"Creating a dask array raised an error. Because the "
+                    f"environment variable DATABROKER_ARRAY_FALLBACK was set"
+                    f"to {switch} we have caught the error and fallen "
+                    f"back to returning a numpy array instead. This may be "
+                    f"very slow. The underlying issue should be resolved. The "
+                    f"error was {err!r}.")
+            else:
+                raise
+
     return out
 
 
