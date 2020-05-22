0afd6021583676750e5aebc070573423a2e3eb36
Thomas A Caswell <tcaswell@bnl.gov>
2020-05-22 13:40:08 -0400

    WIP: work around

--- databroker/tests/test_v2/test_multi_descriptor.py
+++ databroker/tests/test_v2/test_multi_descriptor.py
@@ -11,32 +11,40 @@ data_shape = (1000, 1000)
 @fixture
 def multi_descriptor_doc_stream(request):
     streams = request.param
+
     def doc_gen(stream_names):
 
         # Compose run start
         run_bundle = event_model.compose_run()  # type: event_model.ComposeRunBundle
         start_doc = run_bundle.start_doc
 
-        yield 'start', start_doc
+        yield "start", start_doc
 
         for stream_name in stream_names:
 
             data = np.random.random(data_shape)
 
             # Compose descriptor
-            source = 'NCEM'
-            frame_data_keys = {'raw': {'source': source,
-                                       'dtype': 'number',
-                                       'shape': data.shape}}
-            frame_stream_bundle = run_bundle.compose_descriptor(data_keys=frame_data_keys,
-                                                                name=stream_name,
-                                                                )
-            yield 'descriptor', frame_stream_bundle.descriptor_doc
+            source = "NCEM"
+            frame_data_keys = {
+                "raw": {
+                    "source": source,
+                    "dtype": "array",
+                    "shape": data.shape,
+                    "dims": ("x", "y"),
+                }
+            }
+            frame_stream_bundle = run_bundle.compose_descriptor(
+                data_keys=frame_data_keys, name=stream_name,
+            )
+            yield "descriptor", frame_stream_bundle.descriptor_doc
 
-            yield 'event', frame_stream_bundle.compose_event(data={'raw': data},
-                                                             timestamps={'raw': time.time()})
+            yield "event", frame_stream_bundle.compose_event(
+                data={"raw": data}, timestamps={"raw": time.time()}
+            )
 
-        yield 'stop', run_bundle.compose_stop()
+        yield "stop", run_bundle.compose_stop()
+
     return doc_gen(streams)
 
 
@@ -51,15 +59,21 @@ def _test_ingest_to_xarray(stream):
 
     catalog.upsert(start, stop, doc_gen, [], {})
 
-    assert catalog[-1]['primary'].to_dask()['raw'].compute().shape == (1, *data_shape)
+    assert catalog[-1]["primary"].to_dask()["raw"].compute().shape == (
+        stop["num_events"]["primary"],
+        *data_shape,
+    )
 
 
-@pytest.mark.parametrize("multi_descriptor_doc_stream", (["primary", "baseline"],), indirect=True)
+@pytest.mark.parametrize(
+    "multi_descriptor_doc_stream", (["primary", "baseline"],), indirect=True
+)
 def test_multi_descriptors_unique(multi_descriptor_doc_stream):
     _test_ingest_to_xarray(multi_descriptor_doc_stream)
 
 
-@pytest.mark.parametrize("multi_descriptor_doc_stream", (["primary", "primary"],), indirect=True)
+@pytest.mark.parametrize(
+    "multi_descriptor_doc_stream", (["primary", "primary"],), indirect=True
+)
 def test_multi_descriptors_same(multi_descriptor_doc_stream):
     _test_ingest_to_xarray(multi_descriptor_doc_stream)
-
