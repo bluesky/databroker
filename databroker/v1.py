70483660da022d342f6c064b0db548b0f7fd999e
Pete R Jemian <prjemian@users.noreply.github.com>
2020-07-29 11:45:12 -0500

    ENH v1: access the mongo "dbstats" report

--- databroker/v1.py
+++ databroker/v1.py
@@ -978,6 +978,9 @@ class Broker:
                                   "opening an issue here: "
                                   "https://github.com/bluesky/databroker/issues/new ")
 
+    def stats(self):
+        "Access mongo ``db.collection.stats()`` method"
+        return self.v2.stats()
 
 class Header:
     """
