73267097f7549e169a1f619436af319f18f0117b
Dan Allan <dallan@bnl.gov>
2021-05-10 16:45:52 -0400

    First pass at removing old v2 implementation.

--- databroker/intake_xarray_core/__init__.py
+++
@@ -1,7 +0,0 @@
-import intake  # Import this first to avoid circular imports during discovery.
-from .xarray_container import RemoteXarray
-
-import intake.container
-
-intake.register_driver('databroker-remote-xarray', RemoteXarray)
-intake.container.register_container('databroker-xarray', RemoteXarray)
