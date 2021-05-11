83e14b441a7b93e472029f8983dafcca09f893ee
Dan Allan <dallan@bnl.gov>
2021-05-11 14:59:39 -0400

    TEMP: Hide top-level imports for now.

--- databroker/__init__.py
+++ databroker/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-from .v1 import Broker, Header, ALL, temp, temp_config  # noqa: F401
+# from .v1 import Broker, Header, ALL, temp, temp_config  # noqa: F401
 from .utils import (  # noqa: 401
     lookup_config, list_configs, describe_configs,  # noqa: F401
     wrap_in_doct, DeprecatedDoct, wrap_in_deprecated_doct,  # noqa: F401
@@ -17,13 +17,13 @@ del get_versions
 
 # Legacy imports
 
-try:
-    from .databroker import DataBroker
-except ImportError:
-    pass
-else:
-    from .databroker import (DataBroker,  # noqa: 811
-                             DataBroker as db,
-                             get_events, get_table, stream, get_fields,
-                             restream, process)
-    from .pims_readers import get_images  # noqa: F401
+# try:
+#     from .databroker import DataBroker
+# except ImportError:
+#     pass
+# else:
+#     from .databroker import (DataBroker,  # noqa: 811
+#                              DataBroker as db,
+#                              get_events, get_table, stream, get_fields,
+#                              restream, process)
+#     from .pims_readers import get_images  # noqa: F401
