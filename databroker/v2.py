628ae5015a255c401f506e7d6279fef90f97c79a
Dan Allan <dallan@bnl.gov>
2021-05-14 16:09:46 -0400

    Reinstate databroker.v2 module for back-compat.

---
+++ databroker/v2.py
@@ -0,0 +1,7 @@
+from .client import CatalogOfBlueskyRuns
+
+
+# This is retained for backward-compatibility as it was
+# sometimes used for isinstance checks and can still
+# serve that purpose.
+Broker = CatalogOfBlueskyRuns
