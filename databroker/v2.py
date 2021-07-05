4947c7a787b2b4ad3b1fd581117c79dcaa4e14ee
Dan Allan <dallan@bnl.gov>
2021-07-05 13:38:14 -0400

    Make databroker import fast by delaying import of legacy APIs.

--- databroker/v2.py
+++ databroker/v2.py
@@ -5,3 +5,18 @@ from .client import CatalogOfBlueskyRuns
 # sometimes used for isinstance checks and can still
 # serve that purpose.
 Broker = CatalogOfBlueskyRuns
+
+
+def temp():
+    """
+    Create a temporary Catalog backed by transient storage.
+
+    This is intended for testing, teaching, an demos. The data does not
+    persistent. Do not use this for anything important.
+    """
+    from .mongo_normalized import Catalog
+    from tiled.client import from_catalog
+
+    catalog = Catalog.from_mongomock()  # service-side Catalog
+    client = from_catalog(catalog)  # client-side Catalog
+    return client
