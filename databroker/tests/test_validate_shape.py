172286dde96162f33cde7efb833c9f84f1557559
Hiran Wijesinghe <wwijesing1@bnl.gov>
2024-10-07 17:32:43 -0400

    add tests for default_validate_shape

--- databroker/tests/test_validate_shape.py
+++ databroker/tests/test_validate_shape.py
@@ -1,11 +1,13 @@
 from bluesky import RunEngine
 from bluesky.plans import count
-from ophyd.sim import img
+from ophyd.sim import img, DirectImage
 from tiled.client import Context, from_context
 from tiled.server.app import build_app
 
-from ..mongo_normalized import MongoAdapter
+from ..mongo_normalized import MongoAdapter, BadShapeMetadata
 
+import numpy as np
+import pytest
 
 def test_validate_shape(tmpdir):
     # custom_validate_shape will mutate this to show it has been called
@@ -29,3 +31,63 @@ def test_validate_shape(tmpdir):
         assert not shapes
         client[uid]["primary"]["data"]["img"][:]
         assert shapes
+
+
+@pytest.mark.parametrize("shape,expected_shape",[
+    ( (10,), (11,) ),
+    ( (10,20), (10,21) ),
+    ( (10,20,30), (10,21,30) ),
+    ( (10,20,30), (10,20,31) ),
+    ( (20,20,20,20), (20,21,20,22) ),
+])
+def test_padding(tmpdir, shape, expected_shape):
+    adapter = MongoAdapter.from_mongomock()
+
+    direct_img = DirectImage(
+        func=lambda: np.array(np.ones(shape)), name="direct", labels={"detectors"}
+    )
+    direct_img.img.name = "img"
+
+    with Context.from_app(build_app(adapter), token_cache=tmpdir) as context:
+        client = from_context(context)
+
+        def post_document(name, doc):
+            if name == "descriptor":
+                doc["data_keys"]["img"]["shape"] = expected_shape
+
+            client.post_document(name, doc)
+
+        RE = RunEngine()
+        RE.subscribe(post_document)
+        (uid,) = RE(count([direct_img]))
+        assert client[uid]["primary"]["data"]["img"][0].shape == expected_shape
+
+@pytest.mark.parametrize("shape,expected_shape",[
+    ( (10,), (11,12) ),
+    ( (10,20), (10,200) ),
+    #( (20,20,20,20), (20,21,20,200) ),  # range with page_size=0 because expected byte size is too large
+    #( (10,20), (9,20) ),  # docstring says this should raise BadShapeMetadata, but code doesn't
+])
+def test_default_validate_shape(tmpdir, shape, expected_shape):
+    adapter = MongoAdapter.from_mongomock()
+
+    direct_img = DirectImage(
+        func=lambda: np.array(np.ones(shape)), name="direct", labels={"detectors"}
+    )
+    direct_img.img.name = "img"
+
+    with Context.from_app(build_app(adapter), token_cache=tmpdir) as context:
+        client = from_context(context)
+
+        def post_document(name, doc):
+            if name == "descriptor":
+                doc["data_keys"]["img"]["shape"] = expected_shape
+
+            client.post_document(name, doc)
+
+        RE = RunEngine()
+        RE.subscribe(post_document)
+        (uid,) = RE(count([direct_img]))
+        with pytest.raises(BadShapeMetadata):
+            client[uid]["primary"]["data"]["img"][:]
+
