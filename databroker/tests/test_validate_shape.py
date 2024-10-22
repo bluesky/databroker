10ca0ed0d8de097ce1afdcba08627d7a97d5a1c0
gbischof <bisc8233@gmail.com>
2024-10-22 15:23:40 -0400

    starting to add more tests

--- databroker/tests/test_validate_shape.py
+++ databroker/tests/test_validate_shape.py
@@ -10,42 +10,81 @@ import numpy as np
 import pytest
 
 
-def test_validate_shape(tmpdir):
-    # custom_validate_shape will mutate this to show it has been called
-    shapes = []
+@pytest.mark.parametrize(
+    "shape,expected_shape",
+    [
+        ((10,), (11,)),  # Short by 1, 1d.
+        ((10, 20), (10, 21)),  # Short by 1, 2d.
+        ((10, 20, 30), (10, 21, 30)),  # Short by 1, 3d.
+        ((10, 20, 30), (10, 20, 31)),  # Short by 1, 3d.
+        ((10, 20), (10, 19)),  # Too-big by 1, 2d.
+        ((20, 20, 20, 20), (20, 21, 20, 22)),  # 4d example.
+        # TODO: Need to add some ragged test cases.
+    ],
+)
+def test_padding(tmpdir, shape, expected_shape):
+    adapter = MongoAdapter.from_mongomock()
 
-    def custom_validate_shape(key, data, expected_shape):
-        shapes.append(expected_shape)
-        return data
-
-    adapter = MongoAdapter.from_mongomock(validate_shape=custom_validate_shape)
+    direct_img = DirectImage(
+        func=lambda: np.array(np.ones(shape)), name="direct", labels={"detectors"}
+    )
+    direct_img.img.name = "img"
 
     with Context.from_app(build_app(adapter), token_cache=tmpdir) as context:
         client = from_context(context)
 
         def post_document(name, doc):
+            if name == "descriptor":
+                doc["data_keys"]["img"]["shape"] = expected_shape
+
             client.post_document(name, doc)
 
         RE = RunEngine()
         RE.subscribe(post_document)
-        (uid,) = RE(count([img]))
-        assert not shapes
-        client[uid]["primary"]["data"]["img"][:]
-        assert shapes
+        (uid,) = RE(count([direct_img]))
+        assert client[uid]["primary"]["data"]["img"][0].shape == expected_shape
 
 
 @pytest.mark.parametrize(
     "shape,expected_shape",
     [
-        ((10,), (11,)),
-        ((10, 20), (10, 21)),
-        ((10, 20), (10, 19)),
-        ((10, 20, 30), (10, 21, 30)),
-        ((10, 20, 30), (10, 20, 31)),
-        ((20, 20, 20, 20), (20, 21, 20, 22)),
+        # TODO: Need figure out test cases.
+        #
     ],
 )
-def test_padding(tmpdir, shape, expected_shape):
+def test_default_chunking(tmpdir, shape, expected_shape):
+    adapter = MongoAdapter.from_mongomock()
+
+    direct_img = DirectImage(
+        func=lambda: np.array(np.ones(shape)), name="direct", labels={"detectors"}
+    )
+    direct_img.img.name = "img"
+
+    with Context.from_app(build_app(adapter), token_cache=tmpdir) as context:
+        client = from_context(context)
+
+        def post_document(name, doc):
+            if name == "descriptor":
+                doc["data_keys"]["img"]["shape"] = expected_shape
+
+            client.post_document(name, doc)
+
+        RE = RunEngine()
+        RE.subscribe(post_document)
+        (uid,) = RE(count([direct_img]))
+        assert client[uid]["primary"]["data"]["img"][0].shape == expected_shape
+
+
+@pytest.mark.parametrize(
+    "shape,chunks",
+    [
+        # TODO: Need figure out test cases.
+        # Accepts 'auto'.
+        # suggested_chunks don’t line up perfectly with the data size.
+        # supports chunk_shape in addition to chunks_list.
+    ],
+)
+def test_custom_chunking(tmpdir, shape, expected_shape):
     adapter = MongoAdapter.from_mongomock()
 
     direct_img = DirectImage(
@@ -99,3 +138,27 @@ def test_default_validate_shape(tmpdir, shape, expected_shape):
         (uid,) = RE(count([direct_img]))
         with pytest.raises(BadShapeMetadata):
             client[uid]["primary"]["data"]["img"][:]
+
+
+def test_custom_validate_shape(tmpdir):
+    # custom_validate_shape will mutate this to show it has been called
+    shapes = []
+
+    def custom_validate_shape(key, data, expected_shape):
+        shapes.append(expected_shape)
+        return data
+
+    adapter = MongoAdapter.from_mongomock(validate_shape=custom_validate_shape)
+
+    with Context.from_app(build_app(adapter), token_cache=tmpdir) as context:
+        client = from_context(context)
+
+        def post_document(name, doc):
+            client.post_document(name, doc)
+
+        RE = RunEngine()
+        RE.subscribe(post_document)
+        (uid,) = RE(count([img]))
+        assert not shapes
+        client[uid]["primary"]["data"]["img"][:]
+        assert shapes
