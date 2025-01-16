491a1b303e1526dfc4b1c0adf1d817270f799a91
Dan Allan <dallan@bnl.gov>
2025-01-16 07:53:07 -0500

    Aim entrypoints at definition, not reimport.

--- bluesky-tiled-plugins/bluesky_tiled_plugins/__init__.py
+++ bluesky-tiled-plugins/bluesky_tiled_plugins/__init__.py
@@ -1,3 +1,3 @@
-from ._catalog_of_bluesky_runs import CatalogOfBlueskyRuns  # noqa: F401
-from ._bluesky_event_stream import BlueskyEventStream  # noqa: F401
-from ._bluesky_run import BlueskyRun  # noqa: F401
+from .catalog_of_bluesky_runs import CatalogOfBlueskyRuns  # noqa: F401
+from .bluesky_event_stream import BlueskyEventStream  # noqa: F401
+from .bluesky_run import BlueskyRun  # noqa: F401
