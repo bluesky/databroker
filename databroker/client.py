805135c5e50eec33df7ca7801c5d2c79e1cf055b
Dr. Phil Maffettone <43007690+maffettone@users.noreply.github.com>
2025-02-20 08:35:39 -0500

    fix autoformatting

--- databroker/client.py
+++ databroker/client.py
@@ -1,5 +1,4 @@
 # This module is now a back-compat shim. The objects are defined in a
 # separate package, bluesky_tiled_plugins, that resides in the same
 # git repository as databroker.
-from bluesky_tiled_plugins import (BlueskyEventStream,  # noqa: F401
-                                   BlueskyRun, CatalogOfBlueskyRuns)
+from bluesky_tiled_plugins import BlueskyEventStream, BlueskyRun, CatalogOfBlueskyRuns  # noqa: F401
