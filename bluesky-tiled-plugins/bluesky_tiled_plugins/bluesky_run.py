3b4a73c431aaa4d97cf32def37e325c81aec4174
Eugene M <ymatviych@bnl.gov>
2025-04-09 18:00:46 -0400

    ENH: implement .documents method

--- bluesky-tiled-plugins/bluesky_tiled_plugins/bluesky_run.py
+++ bluesky-tiled-plugins/bluesky_tiled_plugins/bluesky_run.py
@@ -3,7 +3,6 @@ import keyword
 import warnings
 from collections import defaultdict
 from datetime import datetime
-from typing import Optional
 
 import numpy
 import xarray
@@ -25,15 +24,7 @@ _document_types = {
     "stream_datum": None,
 }
 
-RESERVED_KEYS = {"streams", "views", "config", "auxiliary"}
-
-
-def version_selector(version: Optional[str] = None):
-    """Select the appropriate class based on the version."""
-    if (version is None) or version.startswith("1."):
-        return BlueskyRun
-    elif version.startswith("2."):
-        return BlueskyRunV2
+RESERVED_KEYS = {"streams", "views", "config", "aux"}
 
 
 class BlueskyRun(Container):
@@ -43,12 +34,24 @@ class BlueskyRun(Container):
     This adds for bluesky-specific conveniences to the standard client Container.
     """
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Set the version based on the specs
+        self._version = "1.0"  # default version
+        for spec in self.item["attributes"]["specs"]:
+            if spec["name"] == "BlueskyRun":
+                self._version = spec["version"]
+                break
+
+        self._stream_names = set(super().get("streams", ())) if self._version == "2.0" else set(self.keys())
+
     def __repr__(self):
         metadata = self.metadata
         datetime_ = datetime.fromtimestamp(metadata["start"]["time"])
         return (
-            f"<{type(self).__name__} "
-            f"{set(self)!r} "
+            f"<{type(self).__name__} v{self._version} "
+            f"{self._stream_names!r} "
             f"scan_id={metadata['start'].get('scan_id', 'UNSET')!s} "  # (scan_id is optional in the schema)
             f"uid={metadata['start']['uid'][:8]!r} "  # truncated uid
             f"{datetime_.isoformat(sep=' ', timespec='minutes')}"
@@ -79,7 +82,13 @@ class BlueskyRun(Container):
     def v2(self):
         return self
 
-    def documents(self, fill=False):
+    def documents(self, **kwargs):
+        if self._version.startswith("1."):
+            yield from self._documents_from_mongo(fill=kwargs.get("fill", False))
+        elif self._version.startswith("2."):
+            yield from self._documents_from_sql()
+
+    def _documents_from_mongo(self, fill=False):
         # For back-compat with v2:
         if fill == "yes":
             fill = True
@@ -112,138 +121,125 @@ class BlueskyRun(Container):
                 item = json.loads(tail)
                 yield (item["name"], _document_types[item["name"]](item["doc"]))
 
-    def __getattr__(self, key):
-        """
-        Let run.X be a synonym for run['X'] unless run.X already exists.
+    def _documents_from_sql(self):
+        # TODO: Emmit in the right time order; Use event_model classes
 
-        This behavior is the same as with pandas.DataFrame.
-        """
-        # The wisdom of this kind of "magic" is arguable, but we
-        # need to support it for backward-compatibility reasons.
-        if key in IPYTHON_METHODS:
-            raise AttributeError(key)
-        if key in self:
-            return self[key]
-        raise AttributeError(key)
+        yield "start", self.start
 
-    def __dir__(self):
-        # Build a list of entries that are valid attribute names
-        # and add them to __dir__ so that they tab-complete.
-        tab_completable_entries = [
-            entry for entry in self if (entry.isidentifier() and (not keyword.iskeyword(entry)))
-        ]
-        return super().__dir__() + tab_completable_entries
+        # Generate descriptors
+        for desc_name in self._stream_names:
+            desc_node = self["streams"][desc_name]
+            desc_count = desc_node.metadata.get("desc_count", 1)
+            data_keys_names = set(desc_node.keys())
 
-    def describe(self):
-        "For back-compat with intake-based BlueskyRun"
-        warnings.warn(
-            "This will be removed. Use .metadata directly instead of describe()['metadata'].",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return {"metadata": self.metadata}
+            # Assemble dictionaries of data keys and configuration keys
+            data_keys, conf_list = {}, [defaultdict(dict) for _ in range(desc_count)]
+            object_keys = defaultdict(list)
+            conf_node = self["config"].get(desc_name)
+            for item in conf_node.read().to_list() if conf_node else []:
+                data_key = item.pop("data_key")  # Must be present
+                desc_indx = item.pop("desc_indx", 0)
+                value = item.pop("value", None)
+                timestamp = item.pop("timestamp", None)
+                if data_key in data_keys_names:
+                    # This is a proper data_key for internal or external data
+                    data_keys[data_key] = {k: v for k, v in item.items() if v is not None}
+                    object_name = item.get("object_name")
+                    object_keys[object_name].append(data_key)
+                elif value is not None:
+                    # This is a configuration data_key
+                    object_name = item.pop("object_name")
 
-    def __call__(self):
-        warnings.warn(
-            "Do not call a BlueskyRun. For now this returns self, for "
-            "backward-compatibility. but it will be removed in a future "
-            "release.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return self
+                    conf_data = conf_list[desc_indx][object_name].get("data", {})
+                    conf_timestamps = conf_list[desc_indx][object_name].get("timestamps", {})
+                    conf_data_keys = conf_list[desc_indx][object_name].get("data_keys", {})
 
-    def read(self):
-        raise NotImplementedError(
-            "Reading any entire run is not supported. Access a stream in this run and read that."
-        )
+                    conf_data[data_key] = value
+                    conf_timestamps[data_key] = float(timestamp) if timestamp else None
+                    conf_data_keys[data_key] = {k: v for k, v in item.items() if v is not None}
 
-    to_dask = read
+                    conf_list[desc_indx][object_name]["data"] = conf_data
+                    conf_list[desc_indx][object_name]["timestamps"] = conf_timestamps
+                    conf_list[desc_indx][object_name]["data_keys"] = conf_data_keys
 
+            # Fixed part of the descriptor
+            desc_doc = desc_node.metadata.get("extra", {})
+            desc_doc["name"] = desc_name
+            desc_doc["data_keys"] = data_keys
+            desc_doc["object_keys"] = object_keys
 
-class BlueskyRunV2(Container):
-    """
-    This encapsulates the data and metadata for one Bluesky 'run'.
+            # Variable part of the descriptor
+            for desc_indx in range(desc_count):
+                desc_doc["uid"] = conf_node.metadata["descriptors"][desc_indx]["uid"]
+                desc_doc["time"] = float(conf_node.metadata["descriptors"][desc_indx]["time"])
+                desc_doc["configuration"] = conf_list[desc_indx]
 
-    This adds for bluesky-specific conveniences to the standard client Container.
-    """
+                yield "descriptor", desc_doc
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._stream_names = set(super().get("streams", ()))
+        # Generate events
+        for desc_name in self._stream_names:
+            desc_node = self["streams"][desc_name]
+            if "internal" in desc_node.parts:
+                df = desc_node.parts["internal"].read()
+                keys = [k for k in df.columns if k not in {"seq_num", "time"} and not k.startswith("ts_")]
+                for _, row in df.iterrows():
+                    event_doc = {"seq_num": row["seq_num"], "time": float(row["time"])}
+                    event_doc["descriptor"] = desc_node.metadata["uid"]
+                    event_doc["data"] = {k: row[k] for k in keys}
+                    event_doc["timestamps"] = {k: float(row[f"ts_{k}"]) for k in keys}
+                    yield "event", event_doc
 
-    def __repr__(self):
-        metadata = self.metadata
-        datetime_ = datetime.fromtimestamp(metadata["start"]["time"])
-        return (
-            f"<{type(self).__name__} "
-            f"{self._stream_names!r} "
-            f"scan_id={metadata['start'].get('scan_id', 'UNSET')!s} "  # (scan_id is optional in the schema)
-            f"uid={metadata['start']['uid'][:8]!r} "  # truncated uid
-            f"{datetime_.isoformat(sep=' ', timespec='minutes')}"
-            ">"
-        )
+        # Generate Stream Resources and Datums
+        # TODO: needs thorough testing, incl. cases with multiple hdf5 files
+        for desc_name in self._stream_names:
+            desc_node = self["streams"][desc_name]
+            desc_uid = desc_node.metadata["uid"]
+            for data_key in desc_node.parts:
+                if data_key == "internal":
+                    continue
+                sres_uid = f"sr-{desc_uid}-{data_key}"  # can be anything (unique)
+                ds = desc_node[data_key].data_sources()[0]
+                uri = ds.assets[0].data_uri
+                for ast in ds.assets:
+                    if ast.parameter in {"data_uris", "data_uri"}:
+                        uri = ast.data_uri
+                        break
+                sres_doc = {
+                    "data_key": data_key,
+                    "uid": sres_uid,
+                    "run_start": self.start["uid"],
+                    "mimetype": ds.mimetype,
+                    "parameters:": ds.parameters,
+                    "uri": uri,
+                }
+                yield "stream_resource", sres_doc
 
-    @property
-    def start(self):
-        """
-        The Run Start document. A convenience alias:
+                sdat_uid = f"sd-{desc_uid}-{data_key}-0"  # can be anything (unique)
+                total_shape = ds.structure["shape"]
+                datum_shape = desc_node.metadata[data_key]["shape"]
 
-        >>> run.start is run.metadata["start"]
-        True
-        """
-        return self.metadata["start"]
+                max_indx = (
+                    total_shape[0] // datum_shape[0] - 1
+                    if len(total_shape) == len(datum_shape)
+                    else total_shape[0] - 1
+                )
+                sdat_doc = {
+                    "uid": sdat_uid,
+                    "stream_resource": sres_uid,
+                    "descriptor": desc_uid,
+                    "indices": {"start": 0, "stop": max_indx},
+                    "seq_num": {"start": 1, "stop": max_indx + 1},
+                }
+                yield "stream_datum", sdat_doc
 
-    @property
-    def stop(self):
-        """
-        The Run Stop document. A convenience alias:
-
-        >>> run.stop is run.metadata["stop"]
-        True
-        """
-        return self.metadata["stop"]
-
-    @property
-    def v2(self):
-        return self
-
-    def documents(self, fill=False):
-        pass
-        # # For back-compat with v2:
-        # if fill == "yes":
-        #     fill = True
-        # elif fill == "no":
-        #     fill = False
-        # elif fill == "delayed":
-        #     raise NotImplementedError("fill='delayed' is not supported")
-        # else:
-        #     fill = bool(fill)
-        # link = self.item["links"]["self"].replace("/metadata", "/documents", 1)
-        # with self.context.http_client.stream(
-        #     "GET",
-        #     link,
-        #     params={"fill": fill},
-        #     headers={"Accept": "application/json-seq"},
-        # ) as response:
-        #     if response.is_error:
-        #         response.read()
-        #         handle_error(response)
-        #     tail = ""
-        #     for chunk in response.iter_bytes():
-        #         for line in chunk.decode().splitlines(keepends=True):
-        #             if line[-1] == "\n":
-        #                 item = json.loads(tail + line)
-        #                 yield (item["name"], _document_types[item["name"]](item["doc"]))
-        #                 tail = ""
-        #             else:
-        #                 tail += line
-        #     if tail:
-        #         item = json.loads(tail)
-        #         yield (item["name"], _document_types[item["name"]](item["doc"]))
+        yield "stop", self.stop
 
     def __getitem__(self, key):
-        # Process reserved keys:
+        # For v1, return the item directly
+        if self._version.startswith("1."):
+            return super().__getitem__(key)
+
+        # For v2, we need to handle the streams and config keys
         if key in RESERVED_KEYS:
             return super().__getitem__(key)
 
@@ -257,6 +253,12 @@ class BlueskyRunV2(Container):
             return self[key][rest]
 
         return super().__getitem__(key)
+
+    def __iter__(self):
+        if self._version.startswith("1."):
+            return super().__iter__()
+
+        yield from self._stream_names
 
     def __getattr__(self, key):
         """
