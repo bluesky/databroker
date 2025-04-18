8655bc15be001014c0b8f72683ddb97ab18f58f4
Eugene M <ymatviych@bnl.gov>
2025-04-18 16:11:52 -0400

    ENH: add .documents enpoint for json exporter

--- bluesky-tiled-plugins/bluesky_tiled_plugins/exporters.py
+++ bluesky-tiled-plugins/bluesky_tiled_plugins/exporters.py
@@ -3,6 +3,7 @@ import json
 from collections import defaultdict
 
 import awkward
+from tiled.utils import modules_available
 
 
 async def json_exporter(adapter, metadata, filter_for_access):
@@ -13,11 +14,11 @@ async def json_exporter(adapter, metadata, filter_for_access):
         raise ValueError("This exporter only works with BlueskyRun v3.x")
 
     adapter = await filter_for_access(adapter, path_parts=[])
-
-    result = [{"name": "start", "doc": metadata.get("start", {})}]
-    stream_names = await (await adapter.lookup_adapter(["streams"])).keys_range(offset=0, limit=None)
+    yield json.dumps({"name": "start", "doc": metadata.get("start", {})})
+    result = []
 
     # Generate descriptors
+    stream_names = await (await adapter.lookup_adapter(["streams"])).keys_range(offset=0, limit=None)
     for desc_name in stream_names:
         desc_node = await adapter.lookup_adapter(["streams", desc_name])
         conf_node = await adapter.lookup_adapter(["configs", desc_name])
@@ -82,9 +83,19 @@ async def json_exporter(adapter, metadata, filter_for_access):
 
         # Generate events
         if internal_node:
+
+            def format_value(value, precision=6):
+                if hasattr(value, "__array__"):
+                    return [format_value(v) for v in value]
+                elif isinstance(value, float):
+                    if (precision is None) and (value % 1 > 1e-6):
+                        # Do not force "sizable" float to int
+                        return value
+                    return round(value, precision)
+
             df = await internal_node.read()
             keys = [k for k in df.columns if k not in {"seq_num", "time"} and not k.startswith("ts_")]
-            for _, row in df.iterrows():
+            for row in df.to_dict(orient="records"):
                 desc_uid = desc_time_uids[0]["uid"]  # same as desc_node.metadata()["uid"]
                 for _desc_uid_time in desc_time_uids[1:]:
                     if _desc_uid_time["time"] <= row["time"]:
@@ -92,14 +103,15 @@ async def json_exporter(adapter, metadata, filter_for_access):
                 event_doc = {"seq_num": row["seq_num"], "time": row["time"]}
                 event_doc["uid"] = f"event-{desc_uid}-{row['seq_num']}"  # can be anything (unique)
                 event_doc["descriptor"] = desc_uid
-                event_doc["data"] = {k: row[k] for k in keys}
-                event_doc["timestamps"] = {k: row[f"ts_{k}"] for k in keys}
+                event_doc["data"] = {k: format_value(row[k], data_keys[k].get("precision")) for k in keys}
+                event_doc["timestamps"] = {k: format_value(row[f"ts_{k}"], precision=6) for k in keys}
                 result.append({"name": "event", "doc": event_doc})
 
         # Generate Stream Resources and Datums
         # TODO: needs thorough testing, incl. cases with multiple hdf5 files
         desc_uid = desc_node.metadata()["uid"]
         for data_key in part_names:
+            # Loop over data_keys for external data only
             sres_uid = f"sr-{desc_uid}-{data_key}"  # can be anything (unique)
             ds = (await desc_node.lookup_adapter([data_key])).data_sources[0]
             uri = ds.assets[0].data_uri
@@ -117,6 +129,7 @@ async def json_exporter(adapter, metadata, filter_for_access):
             }
             result.append({"name": "stream_resource", "doc": sres_doc})
 
+            # Generate a single stream_datum document for the entire stream
             sdat_uid = f"sd-{desc_uid}-{data_key}-0"  # can be anything (unique)
             total_shape = ds.structure.shape
             datum_shape = desc_node.metadata()[data_key]["shape"]
@@ -135,6 +148,25 @@ async def json_exporter(adapter, metadata, filter_for_access):
             }
             result.append({"name": "stream_datum", "doc": sdat_doc})
 
-    result.append({"name": "stop", "doc": metadata.get("stop", {})})
+    # Make sure that the order of documents is (approximately) correct
+    result = sorted(
+        result,
+        key=lambda x: (
+            x["doc"].get("time", float("inf")),
+            {"stream_resource": 0, "stream_datum": 1}.get(x["name"]),
+        ),
+    )
 
-    return json.dumps(result, indent=2)
+    # Combine events into event_pages
+    if modules_available("databroker"):
+        from databroker.mongo_normalized import batch_documents
+
+        result = [
+            {"name": x[0], "doc": x[1]}
+            for x in batch_documents([(y["name"], y["doc"]) for y in result], size=1000)
+        ]
+
+    for doc in result:
+        yield "\n" + json.dumps(doc)
+
+    yield "\n" + json.dumps({"name": "stop", "doc": metadata.get("stop", {})})
